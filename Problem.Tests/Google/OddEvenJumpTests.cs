81a5f67ba7b1c8b956dae5aca3913996a69b100d
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-24 00:22:37 +0300

    Need to have another look at this solution.

---
+++ Problem.Tests/Google/OddEvenJumpTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Problems.Google;
+using Xunit;
+
+namespace Problem.Tests.Google;
+
+public class OddEvenJumpTests
+{
+    [Theory]
+    [InlineData(new int[] { 10, 13, 12, 14, 15 },2)]
+    [InlineData(new int[] { 2,3,1,1,4},3)]
+    [InlineData(new int[] { 5,1,3,4,2},3)]
+    [InlineData(new int[] { 14,13,15},3)]
+    public void Odd_Even_Jump_Tests(int[] array , int expected)
+    {
+        var result = OddEvenJump.OddEvenJumps(array);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/OddEvenJump.cs
@@ -0,0 +1,58 @@
+﻿namespace Problems.Google;
+
+public static class OddEvenJump
+{
+    public static int OddEvenJumps(int[] arr)
+    {
+        int count = 0;
+        
+        int n = arr.Length;
+        if(n==0)
+            return count;
+        
+        bool[] higher = new bool[n];
+        bool[] lower = new bool[n];
+        int [] nextHighers = GetNextJumps(arr, true);
+        int [] nextLowers = GetNextJumps(arr, false);
+        
+        higher[n-1] = true;
+        lower[n-1] = true;
+        count++;
+        
+        for(int i=n-2;i>=0;i--){
+            int hi = nextHighers[i];
+            int low = nextLowers[i];
+            
+            if(hi > -1) higher[i] = lower[hi];
+            if(low > -1) lower[i] = higher[low];
+            
+            if(higher[i])
+                count++;
+        }
+         
+        return count;
+
+    }
+    
+    private static int[] GetNextJumps(int[] arr, bool high){
+        int[] next = Enumerable.Repeat(-1, arr.Length).ToArray();
+        
+        var sortedList = arr.Select((x, i) => new KeyValuePair<int, int>(x, i));
+        
+        if(high)
+            sortedList = sortedList.OrderBy(x => x.Key).ToList();
+        else
+            sortedList = sortedList.OrderByDescending(x => x.Key).ToList();
+                            
+        Stack<int> stack = new Stack<int>();
+        foreach (var e in sortedList)
+        {
+            while (stack.Count > 0 && stack.Peek() < e.Value)
+                next[stack.Pop()] = e.Value;
+            stack.Push(e.Value);
+        }
+        return next;
+    }
+
+
+}
