4547c54a658f032666e8c1892843a4311fd82e90
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-23 15:57:11 +0300

    Three sum problem

---
+++ Problem.Tests/Array/ThreeSumTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using FluentAssertions;
+using Problems.Array;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Problem.Tests.Array;
+
+public class ThreeSumTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Three_Sum_Tests(int[] input, List<List<int>> expected)
+    {
+        var result = ThreeSum.Execute(input);
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    //[[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
+                    new int[] { -4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6 },
+                    new List<List<int>>
+                    {
+                        new List<int>() {-4, -2, 6}, 
+                        new List<int>() {-4, 0,  4},
+                        new List<int>() {-4, 1,  3},
+                        new List<int>() { -4, 2, 2},
+                        new List<int>() { -2, -2 , 4}, 
+                        new List<int>() { -2, 0, 2 }
+                    }
+                    
+                },
+                new object[]
+                {
+                    new int[] {-2, 0, 1, 1, 2},
+                    new List<List<int>> {new List<int>() {-2, 0, 2}, new List<int>() {-2, 1, 1}}
+                },
+                new object[]
+                {
+                    new int[] {-1, 0, 1, 0},
+                    new List<List<int>> {new List<int>() {-1, 0, 1}}
+                },
+                new object[]
+                {
+                    new int[] {0, 0, 0, 0},
+                    new List<List<int>> {new List<int>() {0, 0, 0}}
+                },
+                new object[]
+                {
+                    new int[] {-1, 0, 1, 2, -1, -4},
+                    new List<List<int>> {new List<int>() {-1, -1, 2}, new List<int>() {-1, 0, 1}}
+                },
+                new object[]
+                {
+                    new int[] { },
+                    new List<List<int>> { }
+                },
+                new object[]
+                {
+                    new int[] {0},
+                    new List<List<int>> { }
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Array/ThreeSum.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Problems.Array;
+
+public static class ThreeSum
+{
+    public static IList<IList<int>> Execute(int[] nums)
+    {
+        IList<IList<int>> triplets = new List<IList<int>>();
+        if (nums.Length < 3) // no triplet can be made
+            return triplets;
+        
+        System.Array.Sort(nums);
+        
+        for (int i = 0; i + 2 < nums.Length; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = nums.Length - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    triplets.Add(new List<int>() { nums[i], nums[j], nums[k] });
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } 
+                else if (sum > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            j = 0;
+            k = nums.Length - 1;
+        }
+        return triplets;
+    }
+    
+}
