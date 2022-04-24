69c34d91efdf5c6b7ffcfe91bb01e067d7718650
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-24 22:31:19 +0300

    License key formatting solved

--- Problems/Google/LicenseKeyFormatting.cs
+++ Problems/Google/LicenseKeyFormatting.cs
@@ -7,6 +7,30 @@ public static class LicenseKeyFormatting
     public static string Execute(string s, int k)
     {
         var sb = new StringBuilder();
+        int n = s.Length;
+        int count = 0;
+        int i = n - 1;
+        while (i >= 0)
+        {
+            var current = char.ToUpper(s[i]);
+            var checkModulo = count % k;
+            if (current == '-')
+            {
+                i--;
+            } 
+            else if (count == k)
+            {
+                sb.Insert(0, '-');
+                count = 0;
+            }
+            else
+            {
+                sb.Insert(0, current);
+                count++;
+                i--;
+            }
+        }
+        
         return sb.ToString();
     }
 }
