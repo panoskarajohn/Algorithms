6acdb58668d74b38f0f9dab9b769eac9f0a3946d
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-25 18:49:08 +0300

    Container with most water solved

---
+++ Problems/Google/Arrays/ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.Google.Arrays;
+
+public static class ContainerWithMostWater
+{
+    public static int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int result = 0;
+
+        while (right > left) 
+        {
+            var distance = right - left;
+            var minHeight = Math.Min(height[right], height[left]);
+            var area = Area(distance, minHeight);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+
+            result = Math.Max(result, area);
+        }
+        
+        return result;
+    }
+
+    private static int Area(int side, int height) => side * height;
+}
