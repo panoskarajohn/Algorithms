d01615a2f33014ae0ce4aadb8fee59604a1996c5
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-27 18:10:17 +0300

    Add binary

---
+++ Problem.Tests/Google/Arrays/AddBinaryTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Problems.Google.Arrays;
+using Problems.Google.String;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class AddBinaryTests
+{
+    [Theory]
+    [InlineData("11", "1", "100")]
+    [InlineData("1010", "1011", "10101")]
+    [InlineData("111", "111", "1110")]
+    public void Add_Binary_Tests(string bin1, string bin2, string expected)
+    {
+        var result = AddBinary.Add(bin1, bin2);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/Arrays/AddBinary.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Problems.Google.Arrays;
+
+public static class AddBinary
+{
+    public static string Add(string a, string b)
+    {
+        var sb = new StringBuilder();
+        int aLength = a.Length - 1;
+        int bLength = b.Length - 1;
+        int carry = 0; 
+
+        while (aLength >= 0 || bLength >= 0)
+        {
+            var x1 = aLength >= 0 ? a[aLength] - '0' : 0;
+            var x2 = bLength >= 0 ? b[bLength] - '0' : 0;
+
+            int sum = carry + x1 + x2;
+
+            sb.Append(sum % 2);
+            carry = sum / 2;
+            
+            aLength--;
+            bLength--;
+        }
+
+        if (carry != 0)
+            sb.Append(carry);
+
+        var res = sb.ToString().ToCharArray();
+        System.Array.Reverse(res);
+        return new string(res);
+    }
+}
