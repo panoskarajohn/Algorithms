fa383e819ccfd19b3a46f19a5c968c5ffbb48cc9
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-27 20:17:39 +0300

    Can jump

---
+++ Problems/Google/Arrays/JumpGame.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems.Google.Arrays;
+
+public class JumpGame
+{
+    public static bool Execute(int[] nums)
+    {
+        var canJumpToEnd = new bool[nums.Length];
+        int n = nums.Length - 1;
+        canJumpToEnd[n] = true;
+        
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (canJumpToEnd[i])
+                continue;
+            else
+            {
+                int jump = nums[i];
+                if (i + jump > n || canJumpToEnd[i + jump])
+                {
+                    canJumpToEnd[i] = true;
+                }
+                else
+                {
+                    for (int j = i + 1; j < i + jump; j++)
+                    {
+                        if (canJumpToEnd[j])
+                            canJumpToEnd[i] = true;
+                    }
+                }
+            }
+        }
+
+        return canJumpToEnd[0];
+    }
+}
