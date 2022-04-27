6769626996e3bdf095777cd6cf9eb3e6b8999927
panoskarajohn <panoskarajohn@hotmail.com>
2022-04-27 19:27:49 +0300

    Image rotation

---
+++ Problem.Tests/Google/Arrays/RotateImageTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Arrays;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class RotateImageTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Rotate_Image_Tests(int[][] rotate, int[][] expected)
+    {
+        RotateImage.RotateBrute(rotate);
+        rotate.Should().BeEquivalentTo(expected);
+    }
+    
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Rotate_Image_No_Allocation_Tests(int[][] rotate, int[][] expected)
+    {
+        RotateImage.Rotate(rotate);
+        rotate.Should().BeEquivalentTo(expected);
+    }
+    
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[][] {new[] {1,2,3} ,new[]{4,5,6},new[]{7,8,9}},
+                    new int[][] {new[] {7,4,1} ,new[]{8,5,2},new[]{9,6,3}},
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Google/Arrays/RotateImage.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Problems.Google.Arrays;
+
+public class RotateImage
+{
+    public static void RotateBrute(int[][] matrix)
+    {
+        var temp = matrix.Select(s => s.ToArray()).ToArray();
+        int n = matrix.Length - 1;
+        for (int row = 0; row < matrix.Length; row++)
+        {
+            for (int col = 0; col < matrix.Length; col++)
+            {
+                matrix[col][n] = temp[row][col];
+            }
+
+            n--;
+        }
+
+    }
+
+    /// <summary>
+    /// No allocation of second temp array
+    /// </summary>
+    /// <param name="matrix"></param>
+    public static void Rotate(int[][] matrix)
+    {
+        int n = matrix.Length;
+
+        for (int i = 0; i < (n + 1) / 2; i++)
+        {
+            for (int j = 0; j < n  / 2; j++)
+            {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
