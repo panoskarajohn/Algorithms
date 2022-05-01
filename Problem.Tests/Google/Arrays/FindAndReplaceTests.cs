e6b6bbd029bd9e8b94731a4d7148d327b63ed834
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-01 16:35:05 +0300

    Find and replace problem

---
+++ Problem.Tests/Google/Arrays/FindAndReplaceTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Problems.Google.Arrays;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class FindAndReplaceTests
+{
+    [Theory]
+    [InlineData("abcd", new []{ 0,2 }, new[] { "a", "cd" }, new[] { "eee", "ffff" }, "eeebffff")]
+    [InlineData("abcd", new []{ 0,2 }, new[] { "ab", "ec" }, new[] { "eee", "ffff" }, "eeecd")]
+    [InlineData("vmokgggqzp", new []{ 3,5,1 }, new[] { "kg", "ggq", "mo" }, new[] { "s", "so", "bfr" }, "vbfrssozp")]
+    [InlineData("abcde", new []{ 2, 2 }, new[] { "cdef", "bc" }, new[] { "f", "fe" }, "abcde")]
+    public void Find_And_Replace_Tests(string s, int[] indices, string[] sources, string[] targets, string expected)
+    {
+        var result = FindAndReplace.FindReplaceString(s, indices, sources, targets);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/Arrays/FindAndReplace.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Problems.Google.Arrays;
+
+public class FindAndReplace
+{
+    public static string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            var index = System.Array.IndexOf(indices, i);
+            
+            if (index != -1)
+            {
+                var sourceAtIndex = sources[index];
+                bool sourceFound = true;
+                for (int j = 0; j < sourceAtIndex.Length; j++)
+                {
+                    if (i + j >= s.Length && j < sourceAtIndex.Length)
+                    {
+                        sourceFound = false;
+                        break;
+                    }
+
+                    if (i + j >= s.Length)
+                        break;
+
+                    if (s[i + j] != sourceAtIndex[j])
+                    {
+                        sourceFound = false;
+                        break;
+                    }
+                }
+
+                if (sourceFound && index < targets.Length)
+                {
+                    sb.Append(targets[index]);
+                    i += sourceAtIndex.Length - 1;
+                }
+                
+                else
+                {
+                    sb.Append(s[i]);
+                }
+            }
+            else
+            {
+                sb.Append(s[i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
