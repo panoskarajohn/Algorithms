40f048a438fcef3250681942640633c02dba4efc
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-02 17:18:50 +0300

    MergeKSortedLists

--- Problem.Tests/LinkedList/MergeKSortedListsTests.cs
+++ Problem.Tests/LinkedList/MergeKSortedListsTests.cs
@@ -23,8 +23,13 @@ public class MergeKSortedListsTests
                 new object[]
                 {
                     new List<ListNode> { ListNodeExtensions.Create(1,4,5), ListNodeExtensions.Create(1,3,4), ListNodeExtensions.Create(2,6) },
-                    ListNodeExtensions.Create(1,1,2,3,4)
-                }
+                    ListNodeExtensions.Create(1,1,2,3,4,4,5,6)
+                },
+                new object[]
+                {
+                    new List<ListNode> { ListNodeExtensions.Create(1,2,2), ListNodeExtensions.Create(1,1,2) },
+                    ListNodeExtensions.Create(1,1,1,2,2,2)
+                },
             };
         }
     }
--- Problems/Google/LinkedList/MergeKSortedLists.cs
+++ Problems/Google/LinkedList/MergeKSortedLists.cs
@@ -4,6 +4,36 @@ public class MergeKSortedLists
 {
     public ListNode MergeKLists(ListNode[] lists)
     {
-        return null;
+        PriorityQueue<ListNode, int> pq = new PriorityQueue<ListNode, int>();
+
+        for (int i = 0; i < lists.Length; i++)
+        {
+            var currentNode = lists[i];
+            while (currentNode != null)
+            {
+                pq.Enqueue(currentNode, currentNode.val);
+                currentNode = currentNode.next;
+            }
+        }
+
+        ListNode head, listNodeToReturn = null;
+
+        if (pq.Count == 0)
+            return null;
+        
+        var node = pq.Dequeue();
+        head = node;
+        listNodeToReturn = head;
+        
+        while (pq.Count != 0)
+        {
+            node = pq.Dequeue();
+            head.next = node;
+            head = head.next;
+        }
+
+        head.next = null;
+
+        return listNodeToReturn;
     }
 }
