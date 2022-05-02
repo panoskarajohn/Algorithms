f445a37a696704d3e2dc6c02c0c9d6a73239c747
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-02 23:29:39 +0300

    Meeting rooms II

---
+++ Problem.Tests/Google/Arrays/MeetingRoomsOne.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Arrays;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class MeetingRoomsTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Meeting_Rooms_One_Tests(int[][] array, bool expected)
+    {
+        var result = MeetingRooms.CanAttendMeetings(array);
+        result.Should().Be(expected);
+    }
+    
+    [Theory, MemberData(nameof(TestDataPropertyRooms))]
+    public void Meeting_Rooms_Two_Tests(int[][] array, int expected)
+    {
+        var result = MeetingRooms.GetRooms(array);
+        result.Should().Be(expected);
+    }
+
+    
+    
+    public static IEnumerable<object[]> TestDataPropertyRooms
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new [] { new []{ 2,11 } , new []{ 6,16 }, new []{ 11,16 }  }, 
+                    2
+                }, 
+                new object[]
+                {
+                    new [] { new []{ 0,30 } , new []{ 5,10 }, new []{ 15,20 }  }, 
+                    2
+                }, 
+                new object[]
+                {
+                    new [] { new[] { 7, 10}, new[] {2, 4} },
+                    1
+                }
+            };
+        }
+    }
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new [] { new []{ 0,30 } , new []{ 5,10 }, new []{ 15,20 }  }, 
+                    false
+                }, 
+                new object[]
+                {
+                    new [] { new[] { 7, 10}, new[] {2, 4} },
+                    true
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Google/Arrays/MeetingRooms.cs
@@ -0,0 +1,48 @@
+﻿namespace Problems.Google.Arrays;
+
+public static class MeetingRooms
+{
+    public static bool CanAttendMeetings(int[][] intervals)
+    {
+        var comparer = Comparer<int>.Default;
+        System.Array.Sort(intervals, (a, b) => comparer.Compare(a[1], b[1]));
+
+        for (int i = 0; i < intervals.Length - 1; i++)
+        {
+            if (i + 1 > intervals.Length)
+                break;
+                
+            var firstInterval = intervals[i];
+            var secondInterval = intervals[i + 1];
+
+            var canAttendMeeting = firstInterval[1] <= secondInterval[0];
+            if (!canAttendMeeting)
+                return false;
+        }
+        return true;
+    }
+
+    public static int GetRooms(int[][] intervals)
+    {
+        if (intervals is null || intervals.Length == 0)
+            return 0;
+        
+        var comparer = Comparer<int>.Default;
+        System.Array.Sort(intervals, (a, b) => comparer.Compare(a[0], b[0]));
+        PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+        
+        heap.Enqueue(intervals[0][1], intervals[0][1]);
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (intervals[i][0] >= heap.Peek())
+            {
+                heap.Dequeue();
+            }
+            heap.Enqueue(intervals[i][1], intervals[i][1]);
+        }
+        
+        return heap.Count;
+
+    }
+}
