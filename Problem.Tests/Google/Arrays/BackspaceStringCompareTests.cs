1cdcb12c04cd14394c28eaa6fc7acb96cb6900f0
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-03 21:35:23 +0300

    BackspaceCompareString

---
+++ Problem.Tests/Google/Arrays/BackspaceStringCompareTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Problems.Google.Arrays;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class BackspaceStringCompareTests
+{
+    [Theory]
+    [InlineData("ab#c", "ad#c", true)]
+    [InlineData("ab##", "c#d#", true)]
+    [InlineData("a#c", "b", false)]
+    public void Backspace_String_Compare_Tests(string s, string t, bool expected)
+    {
+        var result = BackspaceStringCompare.Compare(s, t);
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [InlineData("ab#c", "ad#c", true)]
+    [InlineData("ab##", "c#d#", true)]
+    [InlineData("a#c", "b", false)]
+    public void Backspace_String_Compare_Optimal_Tests(string s, string t, bool expected)
+    {
+        var result = BackspaceStringCompare.CompareOptimal(s, t);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/Arrays/BackspaceStringCompare.cs
@@ -0,0 +1,102 @@
+﻿namespace Problems.Google.Arrays;
+
+public class BackspaceStringCompare
+{
+    /// <summary>
+    /// Brute force solution O(n) but space O(n)
+    /// Question specifically asks for O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static bool Compare(string s, string t)
+    {
+        var stackS = new Stack<char>();
+        var stackT = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '#' && stackS.Count > 0)
+                stackS.Pop();
+            else
+            {
+                if (s[i] != '#')
+                    stackS.Push(s[i]);
+            }
+        }
+        
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i] == '#' && stackT.Count > 0)
+                stackT.Pop();
+            else
+            {
+                if (t[i] != '#')
+                    stackT.Push(t[i]);
+            }
+        }
+
+        if (stackS.Count != stackT.Count)
+            return false;
+
+        return stackS.SequenceEqual(stackT);
+    }
+
+    public static bool CompareOptimal(string s, string t)
+    {
+        int sPointer = s.Length - 1;
+        int tPointer = t.Length - 1;
+        int skipS = 0;
+        int skipT = 0;
+
+        while (tPointer >= 0 || sPointer >= 0)
+        {
+            while (sPointer >= 0)
+            {
+                if (s[sPointer] == '#')
+                {
+                    skipS++;
+                    sPointer--;
+                }
+                else if (skipS > 0)
+                {
+                    skipS--;
+                    sPointer--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            while (tPointer >= 0)
+            {
+                if (t[tPointer] == '#')
+                {
+                    skipT++;
+                    tPointer--;
+                }
+                else if (skipT > 0)
+                {
+                    skipT--;
+                    tPointer--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (sPointer >= 0 && tPointer >= 0 && s[sPointer] != t[tPointer])
+                return false;
+            
+            if (sPointer >= 0 != tPointer >= 0)
+                return false;
+
+            sPointer--;
+            tPointer--;
+        }
+
+        return true;
+    }
+}
