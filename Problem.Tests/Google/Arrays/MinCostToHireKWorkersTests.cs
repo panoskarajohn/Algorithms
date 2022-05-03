863f11cbb9e87573a1eea05f94ce175030c3819b
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-03 23:29:35 +0300

    MinCostToHireKWorkers problem

---
+++ Problem.Tests/Google/Arrays/MinCostToHireKWorkersTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Problems.Array;
+using Xunit;
+
+namespace Problem.Tests.Google.Arrays;
+
+public class MinCostToHireKWorkersTests
+{
+    [Theory]
+    [InlineData(new int[] { 10,20,5 }, new int[] { 70, 50, 30 }, 2, 105.00000)]
+    [InlineData(new int[] { 3,1,10,10,1 }, new int[] { 4,8,2,2,7 }, 3, 30.66667)]
+    public void Min_Cost_To_Hire_K_Workers_Tests(int[] quality, int[] wage, int k, double expected)
+    {
+        var result = MinCostToHIreKWorkers.MinCostToHireWorkers(quality, wage, k);
+        result.Should().BeApproximately(expected, 0.10);
+    }
+}
---
+++ Problems/Array/MinCostToHIreKWorkers.cs
@@ -0,0 +1,36 @@
+﻿namespace Problems.Array;
+
+public class MinCostToHIreKWorkers
+{
+    public static double MinCostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        int n = quality.Length;
+
+        var workers = new List<(double ratio, int quality)>(n);
+        for (int i = 0; i < n; i++)
+        {
+            double ratio = (double)wage[i] / quality[i];
+            workers.Add((ratio, quality[i]));
+        }
+        
+        Comparer<double> comparer = Comparer<double>.Default;
+        workers.Sort((a, b) => comparer.Compare(a.ratio, b.ratio));
+
+        PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+        int sumHeap = 0;
+        double minCost = double.MaxValue;
+
+        foreach (var worker in workers)
+        {
+            heap.Enqueue(-worker.quality, -worker.quality);
+            sumHeap += worker.Item2;
+
+            if (heap.Count > k)
+                sumHeap += heap.Dequeue();
+            if (heap.Count == k)
+                minCost = Math.Min(minCost, sumHeap * worker.ratio);
+        }
+
+        return minCost;
+    }
+}
