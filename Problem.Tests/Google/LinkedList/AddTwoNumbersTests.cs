7d00c188a0f3f393a5e79bcc84e4bf101da14e3b
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-07 20:19:03 +0300

    Add two numbers

---
+++ Problem.Tests/Google/LinkedList/AddTwoNumbersTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.LinkedList;
+using Xunit;
+
+namespace Problem.Tests.Google.LinkedList;
+
+public class AddTwoNumbersTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Add_TwoNumbers_Tests(ListNode l1, ListNode l2, ListNode expected)
+    {
+        var result = AddTwoNumbers.Add(l1, l2);
+        result.IsIdentical(expected)
+            .Should()
+            .BeTrue();
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    ListNodeExtensions.Create(2,4,3), 
+                    ListNodeExtensions.Create(5,6,4),
+                    ListNodeExtensions.Create(7,0,8)
+                },
+                new object[]
+                {
+                    ListNodeExtensions.Create(0), 
+                    ListNodeExtensions.Create(0),
+                    ListNodeExtensions.Create(0)
+                },
+                new object[]
+                {
+                    ListNodeExtensions.Create(9,9,9,9), 
+                    ListNodeExtensions.Create(9,9,9,9,9,9,9),
+                    ListNodeExtensions.Create(8,9,9,9,0,0,0,1)
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/LinkedList/AddTwoNumbers.cs
@@ -0,0 +1,43 @@
+﻿namespace Problems.Google.LinkedList;
+
+public class AddTwoNumbers
+{
+    /// <summary>
+    /// Given two <see cref="ListNode"/> with numbers in reverse order.
+    /// Add them
+    /// </summary>
+    /// <param name="l1"></param>
+    /// <param name="l2"></param>
+    /// <returns></returns>
+    public static ListNode Add(ListNode l1, ListNode l2)
+    {
+        var dummy = new ListNode(0);
+        var l3 = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null)
+        {
+            int l1Val = l1?.val ?? 0;
+            int l2Val = l2?.val ?? 0;
+
+            int sum = l1Val + l2Val + carry;
+            carry = sum / 10;
+            int lastDigit = sum % 10;
+
+            ListNode newNode = new ListNode(lastDigit);
+            l3.next = newNode;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+            l3 = l3.next;
+
+        }
+
+        if (carry > 0)
+            l3.next = new ListNode(carry);
+
+        return dummy.next;
+    }
+
+}
