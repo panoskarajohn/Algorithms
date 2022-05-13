119db5e531235400a064c7e26b0e3080f0a9ae56
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-13 23:36:26 +0300

    Number of components union find

---
+++ Problem.Tests/Google/GraphsTrees/NumberOfComponentsUndirectedGraphTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class NumberOfComponentsUndirectedGraphTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Number_Of_Components_Cndirected_Graph_Tests(int n, int[][] edges, int expected)
+    {
+        var result = NumberOfComponentInAnUndirectedGraph.CountComponents(n, edges);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    5,
+                    new[] {new[] {0, 1}, new[] {1, 2}, new[] {3, 4}},
+                    2
+                },
+                new object[]
+                {
+                    5,
+                    new[] {new[] {0, 1}, new[] {1, 2},new[] {2, 3}, new[] {3, 4}},
+                    1
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/NumberOfComponentInAnUndirectedGraph.cs
@@ -0,0 +1,16 @@
+﻿using Problems.DataStructures;
+
+namespace Problems.Google.GraphsTrees;
+
+public class NumberOfComponentInAnUndirectedGraph
+{
+    public static int CountComponents(int n, int[][] edges)
+    {
+        var uf = new UnionFind(n);
+        foreach (var edge in edges)
+        {
+            uf.Union(edge[0], edge[1]);
+        }
+        return uf.NumberOfComponents;
+    }
+}
