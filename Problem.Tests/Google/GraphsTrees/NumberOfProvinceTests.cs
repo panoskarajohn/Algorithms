8a45c64bf8482883f09d4800cdf86d96de99cfce
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-14 18:53:04 +0300

    Number of provinces problem

---
+++ Problem.Tests/Google/GraphsTrees/NumberOfProvinceTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+using Xunit;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class NumberOfProvinceTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Number_Of_Province_Tests(int[][] isConnected, int expected)
+    {
+        var result = NumberOfProvinces.FindCircleNum(isConnected);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    
+                    new[] {new[] {1,1, 0}, new[] {1, 1, 0}, new[] {0, 0,1}},
+                    2
+                },
+                new object[]
+                {
+                    new[] {new[] {1,0, 0}, new[] {0, 1, 0}, new[] {0, 0,1}},
+                    3
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/NumberOfProvinces.cs
@@ -0,0 +1,25 @@
+﻿using Problems.DataStructures;
+
+namespace Problems.Google.GraphsTrees;
+
+public class NumberOfProvinces
+{
+    public static int FindCircleNum(int[][] isConnected)
+    {
+        int n = isConnected.Length;
+        var uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (isConnected[i][j] == 1)
+                {
+                    uf.Union(i, j);
+                }
+            }
+        }
+        
+        return uf.NumberOfComponents;
+    }
+}
