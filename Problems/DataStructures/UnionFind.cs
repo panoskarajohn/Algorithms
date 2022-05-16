25c6d46cc56fe04287f77de858558802c3b6ef1c
panoskarajohn <panoskarajohn@hotmail.com>
2022-05-16 23:16:11 +0300

    The earliest moment when everyone became friends

--- Problems/DataStructures/UnionFind.cs
+++ Problems/DataStructures/UnionFind.cs
@@ -78,3 +78,52 @@ public class UnionFind
         return true;
     }
 }
+
+public class UnionFindLeet {
+    private int[] _group;
+    private int[] _rank;
+    private int _count;
+    public int Count => _count;
+
+    public UnionFindLeet(int size) {
+        this._group = new int[size];
+        this._rank = new int[size];
+        _count = size;
+        for (int person = 0; person < size; ++person) {
+            this._group[person] = person;
+            this._rank[person] = 0;
+        }
+    }
+        
+    public int Find(int person) {
+        if (this._group[person] != person)
+            this._group[person] = this.Find(this._group[person]);
+        return this._group[person];
+    }
+        
+    public bool Union(int a, int b) {
+        int groupA = this.Find(a);
+        int groupB = this.Find(b);
+        bool isMerged = false;
+
+        // The two people share the same group.
+        if (groupA == groupB)
+            return isMerged;
+
+        // Otherwise, merge the two groups.
+        isMerged = true;
+        // Merge the lower-rank group into the higher-rank group.
+        if (this._rank[groupA] > this._rank[groupB]) {
+            this._group[groupB] = groupA;
+        } else if (this._rank[groupA] < this._rank[groupB]) {
+            this._group[groupA] = groupB;
+        } else {
+            this._group[groupA] = groupB;
+            this._rank[groupB] += 1;
+        }
+
+        _count--;
+
+        return isMerged;
+    }
+}
---
+++ Problems/Google/GraphsTrees/TheEarliestTimeThatEveryoneBecomeFriends.cs
@@ -0,0 +1,31 @@
+﻿using Problems.DataStructures;
+
+namespace Problems.Google.GraphsTrees;
+
+public static class TheEarliestTimeThatEveryoneBecomeFriends
+{
+    public static int EarliestAcq(int[][] logs, int n)
+    {
+        if (logs is null || logs.Length == 0)
+            return -1;
+        
+        // Sort the logs by timestamp
+        var comparer = Comparer<int>.Default;
+        System.Array.Sort(logs, (a, b) => comparer.Compare(a[0], b[0]));
+
+        var uf = new UnionFindLeet(n);
+        int group = n;
+
+        foreach (var log in logs)
+        {
+            if (uf.Union(log[1], log[2]))
+                group--;
+
+            if (group == 1)
+                return log[0];
+        }
+        
+        return -1;
+    }
+    
+}
