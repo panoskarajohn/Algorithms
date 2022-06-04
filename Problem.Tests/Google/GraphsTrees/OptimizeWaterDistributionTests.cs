8534f93d70ed9134e309fd366ff5f6e54983536a
panoskarajohn <panoskarajohn@hotmail.com>
2022-06-04 22:08:58 +0300

    Optimize water distribution

---
+++ Problem.Tests/Google/GraphsTrees/OptimizeWaterDistributionTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+using Xunit;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class OptimizeWaterDistributionTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Optimize_Water_Distribution_Tests(int n, int[] wells, int[][] pipes, int expected)
+    {
+        var result = OptimizeWaterDistribution.MinCostToSupplyWater(n, wells, pipes);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    3,
+                    new[] {1, 2, 2},
+                    new[] {new[] {1, 2, 1}, new[] {2, 3, 1}},
+                    3
+                },
+                new object[]
+                {
+                    2,
+                    new[] {1, 1},
+                    new[] {new[] {1, 2, 1}, new[] {1, 2, 2}},
+                    2
+                },
+                new object[]
+                {
+                    5,
+                    new[] {46012, 72474, 64965, 751, 33304},
+                    new[] {new[] {2, 1, 6719}, new[] {3, 2, 75312}, new[] {5, 3, 44918}},
+                    131704
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/OptimizeWaterDistribution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Problems.DataStructures;
+
+namespace Problems.Google.GraphsTrees;
+
+public static class OptimizeWaterDistribution
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n">Number of houses</param>
+    /// <param name="wells">Cost for ith house well</param>
+    /// <param name="pipes">house1 [0], house2[1], cost[2]</param>
+    /// <returns></returns>
+    public static int MinCostToSupplyWater(int n, int[] wells, int[][] pipes)
+    {
+        var uf = new UnionFindLeet(n + wells.Length);
+        var edges = new List<(int cost, int u, int v)>();
+
+        // Create virtual nodes
+        for (int i = 0; i < wells.Length; i++)
+        {
+            edges.Add((wells[i], 0, i + 1));
+        }
+
+        foreach (var pipe in pipes)
+        {
+            edges.Add((pipe[2], pipe[0], pipe[1]));
+        }
+        
+        Comparer<(int cost,int u,int v)> comparer = Comparer<(int cost, int u, int v)>.Default;
+        edges.Sort((x,y) => x.cost.CompareTo(y.cost));
+        
+        int cost = 0;
+        //build dummy edge from each house to the dummy well
+        foreach (var edge in edges)
+        {
+            if (uf.Union(edge.u, edge.v))
+            {
+                cost += edge.cost;
+            }
+        }
+        return cost;
+    }
+}
