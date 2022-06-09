d32a10f1a28dd4591d187b2530cbcf8e964fe4b5
panoskarajohn <panoskarajohn@hotmail.com>
2022-06-09 21:56:58 +0300

    Reconstruct Itinerary

---
+++ Problem.Tests/Google/GraphsTrees/ReconstructItineraryTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+using Xunit;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class ReconstructItineraryTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Reconstruct_Itinerary_Tests(IList<IList<string>> data, IList<string> expected)
+    {
+        var actual = new ReconstructItinerary().FindItinerary(data);
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new List<IList<string>>()
+                    {
+                        new List<string>() { "MUC", "LHR" },
+                        new List<string>() { "JFK", "MUC" },
+                        new List<string>() { "SFO", "SJC" },
+                        new List<string>() { "LHR", "SFO" }
+                        
+                    },
+                    new List<string>() { "JFK", "MUC", "LHR", "SFO", "SJC" }
+                },
+                new object[]
+                {
+                    new List<IList<string>>()
+                    {
+                        new List<string>() { "JFK", "SFO" },
+                        new List<string>() { "JFK", "ATL" },
+                        new List<string>() { "SFO", "ATL" },
+                        new List<string>() { "ATL", "JFK" },
+                        new List<string>() { "ATL", "SFO" }
+                    },
+                    new List<string>() { "JFK", "ATL", "JFK", "SFO", "ATL", "SFO" }
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/ReconstructItinerary.cs
@@ -0,0 +1,89 @@
+﻿namespace Problems.Google.GraphsTrees;
+
+public class ReconstructItinerary
+{
+    /// <summary>
+    /// The graph
+    /// </summary>
+    private Dictionary<string, List<string>> _flightMap =
+        new Dictionary<string, List<string>>();
+
+    private Dictionary<string, bool[]> _visitedMap = new Dictionary<string, bool[]>();
+
+    private int _flights = 0;
+    private List<string> _result = new List<string>();
+    
+
+
+    public IList<string> FindItinerary(IList<IList<string>> tickets)
+    {
+        //build the graph
+        foreach (var ticket in tickets)
+        {
+            var flightFrom = ticket[0];
+            var flightTo = ticket[1];
+
+            if (_flightMap.ContainsKey(flightFrom))
+            {
+                _flightMap[flightFrom].Add(flightTo);
+            }
+            else
+            {
+                _flightMap[flightFrom] = new List<string> {flightTo};
+            }
+        }
+        
+        // sort the destinations and init the visited map
+        foreach (var _flight in _flightMap)
+        {
+            _flight.Value.Sort();
+            _visitedMap[_flight.Key] = new bool[_flight.Value.Count];
+        }
+        
+        // start from JFK
+        _flights = tickets.Count;
+        LinkedList<string> routes = new LinkedList<string>();
+        routes.AddLast("JFK");
+        
+        // start the DFS
+        Backtracking("JFK", routes);
+        
+        return _result;
+    }
+
+    private bool Backtracking(string origin, LinkedList<string> routes)
+    {
+        if (routes.Count == _flights + 1)
+        {
+            _result = routes.ToList();
+            return true;
+        }
+
+        if (!_flightMap.ContainsKey(origin))
+            return false;
+
+        int index = 0;
+        bool[] bitmap = _visitedMap[origin];
+
+        foreach (var destination in _flightMap[origin])
+        {
+            if (!bitmap[index])
+            {
+                bitmap[index] = true;
+                routes.AddLast(destination);
+                bool ret = Backtracking(destination, routes);
+                
+                routes.RemoveLast();
+                bitmap[index] = false;
+                
+                if (ret)
+                    return true;
+                
+            }
+
+            index++;
+        }
+
+        return false;
+    }
+}
