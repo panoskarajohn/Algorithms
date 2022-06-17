c41b9617d5e47582249dc75eecb62d2a8d34aca1
panoskarajohn <panoskarajohn@hotmail.com>
2022-06-17 15:45:08 +0300

    Rotting oranges

---
+++ Problem.Tests/Google/GraphsTrees/RottingOrangesTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Problems.Google.GraphsTrees;
+using Xunit;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class RottingOrangesTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Rotting_Oranges_Tests(int[][] data, int expected)
+    {
+        var result = new RottingOranges().OrangesRotting(data);
+        Assert.Equal(expected, result);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {2, 1, 1}, new[] {1, 1, 0}, new[] {0, 1, 1}},
+                    4
+                },
+                new object[]
+                {
+                    new[] {new[] {2, 1, 1}, new[] {0, 1, 1}, new[] {1, 0, 1}},
+                    -1
+                },
+                new object[]
+                {
+                    new[] {new[] {0, 2}},
+                    0
+                },
+                new object[]
+                {
+                    new[] {new[] {1}},
+                    -1
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 2, 2}},
+                    1
+                },
+                new object[]
+                {
+                    new[] {new[] {0}},
+                    0
+                },
+                new object[]
+                {
+                    new[] {new[] {2, 1, 1}, new[] {1, 1, 1}, new[] {0, 1, 2}},
+                    2
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/RottingOranges.cs
@@ -0,0 +1,98 @@
+﻿namespace Problems.Google.GraphsTrees;
+
+public class RottingOranges
+{
+    /// <summary>
+    /// 4-directionally adjacent directions
+    /// </summary>
+    public int OrangesRotting(int[][] grid)
+    {
+        int time = -1;
+        int freshOranges = 0;
+        var queue = new Queue<(int row, int col)>();
+        
+        for(int i = 0; i < grid.Length; i++)
+        {
+            for(int j = 0; j < grid[0].Length; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    queue.Enqueue((i, j));
+                }
+                
+                if(grid[i][j] == 1)
+                {
+                    freshOranges++;
+                }
+            }
+        }
+        
+        queue.Enqueue((-1 , -1));
+        
+        if (freshOranges == 0)
+            return 0;
+
+        while (queue.Any())
+        {
+            var poll = queue.Dequeue();
+
+            if (poll.row == -1)
+            {
+                time++;
+                if(queue.Any())
+                {
+                    queue.Enqueue((-1, -1));
+                }
+                continue;
+            }
+            
+            var neighbors = GetNeighbors(poll.row, poll.col, grid);
+
+            foreach (var neighbor in neighbors)
+            {
+                if (grid[neighbor.row][neighbor.col] == 1)
+                {
+                    grid[neighbor.row][neighbor.col] = 2;
+                    queue.Enqueue(neighbor);
+                    freshOranges--;
+                }
+            }
+        }
+        
+        if(freshOranges> 0)
+            return -1;
+        
+        return time;
+    }
+
+    private IList<(int row, int col)> GetNeighbors(int row, int col, int[][] grid)
+    {
+        int[][] directions = new int[][]
+        {
+            new[] {-1, 0},
+            new[] {0, -1},
+            new[] {0, 1},
+            new[] {1, 0}
+        };
+        var neighbors = new List<(int row ,int col)>();
+
+        foreach (var direction in directions)
+        {
+            var newRow = row + direction[0];
+            var newCol = col + direction[1];
+            
+            if (newRow < 0 || 
+                newRow >= grid.Length || 
+                newCol < 0 || 
+                newCol >= grid[0].Length)
+            {
+                continue;
+            }
+            
+            neighbors.Add((newRow, newCol));
+            
+        }
+        
+        return neighbors;
+    }
+}
