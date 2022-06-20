0fafedf8293c21a9b4756fdf76b31c33333bdc35
panoskarajohn <panoskarajohn@hotmail.com>
2022-06-20 23:09:05 +0300

    Network delay

---
+++ Problem.Tests/Google/GraphsTrees/NetworkDelayTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Problems.Google.GraphsTrees;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class NetworkDelayTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Network_Delay_Dijkstra_Tests(int[][] data, int n, int k, int expected)
+    {
+        var networkDelay = new NetworkDelay();
+        var result = networkDelay.NetworkDelayTime(data, n, k);
+        Assert.Equal(expected, result);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {2, 1, 1}, new[] {2, 3, 1}, new[] {3, 4, 1}},
+                    4,
+                    2,
+                    2
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 2, 1}},
+                    2,
+                    1,
+                    1
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 2, 1}},
+                    2,
+                    2,
+                    -1
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/NetworkDelay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace Problems.Google.GraphsTrees;
+
+public class NetworkDelay
+{
+    public int NetworkDelayTime(int[][] times, int n, int k)
+    {
+        List<(int node, int weight)>[] adjList = new List<(int node, int weight)>[n+1];
+        
+        for(var i =0; i<= n; i++ ){
+            adjList[i] = new List<(int node, int weight)>();
+        }
+        
+        foreach( var time in times){
+            adjList[time[0]].Add((time[1],time[2]));
+        }
+        
+        bool[] visited = new bool[n+1];
+        
+        PriorityQueue<int,int> pq = new PriorityQueue<int,int>();
+        
+        pq.Enqueue(k,0);
+        int result = 0;
+        int visitedCount = 0;
+        while(pq.TryDequeue(out int node, out int weight)){
+            if(visited[node]) continue;
+            visitedCount++;
+            visited[node] = true;
+            result = Math.Max(result,weight);
+            foreach(var adj in adjList[node]){
+                var totalWeight = weight + adj.weight;
+                pq.Enqueue(adj.node,totalWeight);
+            }
+        }
+        return visitedCount == n ? result : -1;
+    }
+        
+}
+    
