a112c2812f567077a97d92e94cd6e80a46b298bf
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-05 10:04:05 +0300

    word ladder

---
+++ Problem.Tests/Google/GraphsTrees/WordLadderTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class WordLadderTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Word_Ladder_Tests(string beginWord, string endWord, IList<string> wordList, int expected)
+    {
+        var result = new WordLadder().LadderLength(beginWord, endWord, wordList);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "hit",
+                    "cog", 
+                    new List<string> { "hot","dot","dog","lot","log","cog" },
+                    5
+                },
+                new object[]
+                {
+                    "hit",
+                    "cog", 
+                    new List<string> { "hot","dot","dog","lot","log" },
+                    0
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/WordLadder.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Problems.Google.GraphsTrees;
+
+public class WordLadder
+{
+    private Dictionary<char, Dictionary<char, int>> _graph = new();
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+        if (!wordList.Contains(endWord))
+            return 0;
+        var queue = new Queue<string>();
+        var words = new HashSet<string>(wordList);
+
+        words.Remove(beginWord);
+        queue.Enqueue(beginWord);
+
+        int level = 0;
+
+        while (queue.Any())
+        {
+            int size = queue.Count;
+            level++;
+            for (int i = 0; i < size; i++)
+            {
+                string current = queue.Dequeue();
+                if (current == endWord) return level;
+
+                IList<string> neighbors = GetNeighbors(current);
+                foreach (var neighbor in neighbors)
+                {
+                    if (words.Contains(neighbor))
+                    {
+                        words.Remove(neighbor);
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private IList<string> GetNeighbors(string wordNode)
+    {
+        char[] chars = wordNode.ToCharArray();
+        var result = new List<string>();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char temp = chars[i];
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                chars[i] = ch;
+                string neighbor = new string(chars);
+                result.Add(neighbor);
+            }
+
+            chars[i] = temp;
+        }
+
+        return result;
+    }
+
+
+}
