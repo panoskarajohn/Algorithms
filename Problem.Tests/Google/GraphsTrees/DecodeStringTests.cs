d81631c675cba9a1c58e10d24cd197fc1e866225
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-05 21:41:40 +0300

    decode string

---
+++ Problem.Tests/Google/GraphsTrees/DecodeStringTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+
+namespace Problem.Tests.Google.GraphsTrees;
+public class DecodeStringTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Decode_String_Tests(string s, string expected)
+    {
+        var result = new DecodeString().Decode(s);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "3[a]2[bc]",
+                    "aaabcbc"
+                },
+                new object[]
+                {
+                    "3[a2[c]]",
+                    "accaccacc"
+                },
+                new object[]
+                {
+                    "2[abc]3[cd]ef",
+                    "abcabccdcdcdef"
+                },
+
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/DecodeString.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Problems.Google.GraphsTrees;
+
+public class DecodeString
+{
+    public string Decode(string s)
+    {
+        int n = s.Length;
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == ']')
+            {
+                var decodedString = new List<char>();
+                
+                //get the encoding string
+                while (stack.Peek() != '[')
+                {
+                    decodedString.Add(stack.Pop());
+                }
+                
+                //pop the [
+                stack.Pop();
+                int baseOfTen = 1;
+                int multiplier = 0;
+
+                while (stack.Any() && char.IsDigit(stack.Peek()))
+                {
+                    multiplier += (stack.Pop() - '0') * baseOfTen;
+                    baseOfTen *= 10;
+                }
+
+                while (multiplier != 0)
+                {
+                    // append the decoded string to the stack
+                    for (int j = decodedString.Count - 1; j >= 0; j--)
+                    {
+                        stack.Push(decodedString[j]);
+                    }
+
+                    multiplier--;
+                }
+            }
+            else
+            {
+                stack.Push(s[i]);
+            }
+        }
+        
+        char[] result = new char[stack.Count];
+        for (int i = result.Length - 1; i >= 0; i--)
+        {
+            result[i] = stack.Pop();
+        }
+
+        return new string(result);
+    }
+}
