953f2dbaf6670231263bb9dcc75d8eab116829d4
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-05 10:03:55 +0300

    Parallel courses

---
+++ Problem.Tests/Google/GraphsTrees/ParallelCoursesTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+
+public class ParallelCoursesTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Parallel_Courses_Tests(int n, int[][] relations, int expected)
+    {
+        var result = new ParallelCourses().MinNumberOfSemesters(n, relations);
+        result.Should().Be(expected);
+    }
+    
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Parallel_Courses_Dfs_Tests(int n, int[][] relations, int expected)
+    {
+        var result = new ParallelCourses().MinNumberOfSemestersDfs(n, relations);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    3,
+                    new[] {new[] {1, 3}, new[] {2, 3}},
+                    2
+                },
+                new object[]
+                {
+                    3,
+                    new[] {new[] {1, 2}, new[] {2,3}, new []{3,1}},
+                    -1
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/ParallelCourses.cs
@@ -0,0 +1,155 @@
+﻿namespace Problems.Google.GraphsTrees;
+
+public class ParallelCourses
+{
+    #region Kahn's Algorithm
+
+    public int MinNumberOfSemesters(int n, int[][] relations)
+    {
+        var graph = CreateGraph(n, relations, out Dictionary<int, int> inDegree);
+        var semesters = 0;
+        var numberOfClasses = 0;
+
+        var zeroDegree = BuildZeroDegreeQueue(inDegree);
+
+        while (zeroDegree.Any())
+        {
+            semesters++;
+            var newQueue = new Queue<int>();
+
+            foreach (var zero in zeroDegree)
+            {
+                numberOfClasses++;
+                foreach (var neighbor in graph[zero])
+                {
+                    inDegree[neighbor]--;
+                    if (inDegree[neighbor] == 0)
+                    {
+                        newQueue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            zeroDegree = newQueue;
+        }
+        return numberOfClasses == n ? semesters : -1;
+    }
+
+    private Queue<int> BuildZeroDegreeQueue(Dictionary<int, int> inDegree)
+    {
+        var zeroDegree = new Queue<int>();
+
+        foreach (var i in inDegree)
+        {
+            if(i.Value == 0)
+                zeroDegree.Enqueue(i.Key);
+        }
+        
+        return zeroDegree;
+    }
+
+    private Dictionary<int, HashSet<int>> CreateGraph(int n, int[][] relations, out Dictionary<int, int> inDegree)
+    {
+        var adjacencyList = new Dictionary<int, HashSet<int>>();
+        inDegree = new Dictionary<int, int>();
+        for (int i = 1; i < n + 1; i++) //problem never starts from 0 no need to check that
+        {
+            inDegree.Add(i, 0);
+            adjacencyList.Add(i, new HashSet<int>());
+        }
+        
+        foreach (var relation in relations)
+        {
+            adjacencyList[relation[0]].Add(relation[1]);
+            inDegree[relation[1]]++;
+        }
+        return adjacencyList;
+    }
+
+    #endregion
+    
+    #region DFS
+
+    public int MinNumberOfSemestersDfs(int n, int[][] relations)
+    {
+        var graph = BuildDfsGraph(n, relations);
+        // We need to break the problem into two parts
+        //Detect a cycle
+        int[] visited = new int[n + 1];
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (CheckDfsCycle(i, graph, visited) == -1)
+                return -1;
+        }
+        
+        //We have not detected a cycle
+        //Calculate the length of the longest path
+        int[] visitedLength = new int[n + 1];
+        int maxLength = 1;
+        for (int i = 1; i < n + 1; i++)
+        {
+            int length = DfsMaxPath(i, graph, visitedLength);
+            maxLength = Math.Max(length, maxLength);
+        }
+        
+        return maxLength;
+    }
+
+    private int DfsMaxPath(int node, Dictionary<int, HashSet<int>> graph, int[] visitedLength)
+    {
+        if (visitedLength[node] != 0)
+            return visitedLength[node];
+
+        int max = 1;
+        foreach (var neighbors in graph[node])
+        {
+            int length = DfsMaxPath(neighbors, graph, visitedLength);
+            max = Math.Max(length + 1, max);
+        }
+
+        visitedLength[node] = max;
+        return max;
+    }
+
+    private int CheckDfsCycle(int node, Dictionary<int, HashSet<int>> graph, int[] visited)
+    {
+        if (visited[node] != 0)
+        {
+            return visited[node];
+        }
+        else
+        {
+            visited[node] = -1; //mark as visiting
+        }
+
+        foreach (var neighbor in graph[node])
+        {
+            if (CheckDfsCycle(neighbor, graph, visited) == -1)
+            {
+                return -1;
+            }
+        }
+        
+        visited[node] = 1; //mark as visited
+        return 1;
+    }
+
+    private Dictionary<int, HashSet<int>> BuildDfsGraph(int n, int[][] relations)
+    {
+        Dictionary<int, HashSet<int>> adj = new();
+        for (int i = 1; i < n + 1; i++)
+        {
+            adj.Add(i, new HashSet<int>());
+        }
+
+        foreach (var relation in relations)
+        {
+            adj[relation[0]].Add(relation[1]);
+        }
+
+        return adj;
+    }
+
+    #endregion
+    
+}
