9ee8865684767c7f73cbaff2bad9ed67e97b14f8
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-08 19:01:07 +0300

    Crack Safe

---
+++ Problem.Tests/Google/GraphsTrees/CrackSafeTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.GraphsTrees;
+
+namespace Problem.Tests.Google.GraphsTrees;
+
+public class CrackSafeTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Crack_Safe_Tests(int n, int k, string expected)
+    {
+        var result = new CrackSafe().Crack(n, k);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    1,2, "10"
+                },
+                new object[]
+                {
+                    2,2,"01100"
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/GraphsTrees/CrackSafe.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace Problems.Google.GraphsTrees;
+
+public class CrackSafe
+{
+    /*
+     * The solution that was showcased was with Euler's path
+     * This problem needs to be revisited
+     * Hard problem
+     * Hint:
+     * We can think of this problem as the problem of finding an Euler path (a path visiting every edge exactly once)
+     * on the following graph: there are $$k^{n-1}$$ nodes with each node having $$k$$ edges.
+     * It turns out this graph always has an Eulerian circuit
+     * (path starting where it ends.) We should visit each node in "post-order" so as to not get stuck in the graph prematurely.
+     */
+    private HashSet<string> _seen;
+    private StringBuilder _answer;
+    /// <summary>
+    /// There is a safe protected by a password.
+    /// The password is a sequence of n digits where each digit can be in the range [0, k - 1].
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public string Crack(int n, int k)
+    {
+        if (n == 0 && k == 0)
+            return "0";
+
+        _seen = new HashSet<string>();
+        _answer = new StringBuilder();
+
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < n - 1; i++)
+            sb.Append("0");
+
+        var start = sb.ToString();
+        
+        Dfs(start,k);
+
+        _answer.Append(start);
+
+        return _answer.ToString();
+    }
+    
+    void Dfs(string node, int k)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            var current = node + i;
+            if (!_seen.Contains(current))
+            {
+                _seen.Add(current);
+                Dfs(current.Substring(1), k);
+
+                _answer.Append(i);
+            }
+        }
+    }
+}
