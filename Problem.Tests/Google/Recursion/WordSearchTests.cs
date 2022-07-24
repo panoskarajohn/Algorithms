258bca15766411f331841d4e8e50724a6bf2850e
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-24 23:02:30 +0300

    WordSearch

---
+++ Problem.Tests/Google/Recursion/WordSearchTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Recursion;
+
+namespace Problem.Tests.Google.Recursion;
+
+
+public class WordSearchTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Word_search_tests(char[][] board, string word, bool expected)
+    {
+        var result = new WordSearch().Exists(board, word);
+        result.Should().Be(expected);
+    }
+
+    [Theory, MemberData(nameof(TestDataTwoProperty))]
+    public void Word_Search_Two_Slow_Tests(char[][] board, string[] words, string[] expected)
+    {
+        var result = new WordSearch().FindWords(board, words);
+        result.Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataTwoProperty
+    {
+        get
+        {
+            return new[]
+            {
+              new object[]
+              {
+                  new char[][]
+                  {
+                      new[] {'o', 'a', 'a', 'n'},
+                      new[] {'e', 't', 'a', 'e'},
+                      new[] {'i', 'h', 'k', 'r'},
+                      new[] {'i', 'f', 'l', 'v'},
+                  },
+                  new string[] { "oath", "pea", "eat", "rain" }, 
+                  new string[] { "eat", "oath" }
+              }
+            };
+        }
+    }
+
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                  new char[][]
+                  {
+                      new[] { 'A' , 'B' },
+                      new[] {'C', 'D'}
+                  },
+                  "ACDB",
+                  true
+                },
+                new object[]
+                {
+                    new char[][]
+                    {
+                        new[] {'A'},
+
+                    },
+                    "A",
+                    true
+                    
+                },
+                new object[]
+                {
+                    new char[][]
+                    {
+                        new[] {'A', 'B', 'C', 'E'},
+                        new[] {'S', 'F', 'C', 'S'},
+                        new[] {'A', 'D', 'E', 'E'},
+                    },
+                    "ABCCED",
+                    true
+                    
+                },
+                new object[]
+                {
+                    new char[][]
+                    {
+                        new[] {'A', 'B', 'C', 'E'},
+                        new[] {'S', 'F', 'C', 'S'},
+                        new[] {'A', 'D', 'E', 'E'},
+                    },
+                    "SEE",
+                    true
+                },
+                new object[]
+                {
+                    new char[][]
+                    {
+                        new[] {'A', 'B', 'C', 'E'},
+                        new[] {'S', 'F', 'C', 'S'},
+                        new[] {'A', 'D', 'E', 'E'},
+                    },
+                    "ABCB",
+                    false
+                },
+                
+            };
+        }
+    }
+}
---
+++ Problems/Google/Recursion/WordSearch.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+
+namespace Problems.Google.Recursion;
+
+public class WordSearch
+{
+    private readonly int[][] _directions = new int[][]
+    {
+        new int[] { -1, 0 },
+        new int[] { 0, 1 },
+        new int[] { 1, 0 },
+        new int[] { 0, -1 }
+    };
+
+    private int _sizeRow;
+    private int _sizeCol;
+    private char[][] _board;
+
+    /// <summary>
+    /// And of course this throws a time limit exceeded
+    /// </summary>
+    /// <param name="board"></param>
+    /// <param name="words"></param>
+    /// <returns></returns>
+    public IList<string> FindWords(char[][] board, string[] words)
+    {
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            if(Exists(board,word))
+                result.Add(word);
+        }
+
+        return result;
+    }
+
+    public bool Exists(char[][] board, string word)
+    {
+        _sizeRow = board.Length;
+        _sizeCol = board[0].Length;
+        _board = board;
+
+        if (_sizeCol * _sizeRow < word.Length)
+            return false;
+
+        if (_sizeCol == 1 && _sizeRow == 1)
+        {
+            if (word.Length == 1)
+            {
+                return word[0] == _board[0][0];
+            }
+        }
+        
+        for (int i = 0; i < _sizeRow; i++)
+        {
+            for (int j = 0; j < _sizeCol; j++)
+            {
+                if (Exists( i, j, word, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool Exists(int row, int col ,string word, int step)
+    {
+        if (step >= word.Length)
+            return true;
+
+        if (_board[row][col] != word[step])
+            return false;
+
+        bool ret = false;
+        _board[row][col] = '#';
+
+        foreach (var direction in GetDirections(row, col))
+        {
+            ret = Exists(direction.row, direction.col, word, step + 1);
+            if (ret) break;
+        }
+
+        _board[row][col] = word[step];
+        return ret;
+    }
+
+    private List<(int row, int col)> GetDirections(int row, int col)
+    {
+        var result = new List<(int row, int col)>();
+
+        foreach (var direction in _directions)
+        {
+            var newRow = row + direction[0];
+            var newCol = col + direction[1];
+            
+            if(newRow < 0 || newRow >= _sizeRow)
+                continue;
+            
+            if(newCol < 0 || newCol >= _sizeCol)
+                continue;
+            
+            result.Add((newRow, newCol));
+        }
+
+        return result;
+
+    }
+}
