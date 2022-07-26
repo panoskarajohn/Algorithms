c7cd7e40cc8ce52cc80be01d217ea926035d1c43
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-26 11:29:50 +0300

    Letter combinations

---
+++ Problem.Tests/Google/Recursion/LetterCombinationsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Recursion;
+
+namespace Problem.Tests.Google.Recursion;
+
+
+public class LetterCombinationsTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Letter_combinations_tests(string digits, IList<string> expected)
+    {
+        var combinations = new LetterCombinations().Get(digits);
+        combinations.Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "23",
+                    new List<string> {"ad","ae","af","bd","be","bf","cd","ce","cf"} 
+                },
+                new object[]
+                {
+                    "2",
+                    new List<string> { "a", "b", "c" }
+                },
+                new object[]
+                {
+                    "", 
+                    new List<string> {}
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Google/Recursion/LetterCombinations.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Problems.Google.Recursion;
+
+public class LetterCombinations
+{
+    private readonly IReadOnlyDictionary<char, char[]> _phoneKeys = new Dictionary<char, char[]>()
+    {
+        {'2', new char[]{ 'a', 'b', 'c' }},
+        {'3', new char[]{ 'd', 'e', 'f' }},
+        {'4', new char[]{ 'g', 'h', 'i' }},
+        {'5', new char[]{ 'j', 'k', 'l' }},
+        {'6', new char[]{ 'm', 'n', 'o' }},
+        {'7', new char[]{ 'p', 'q', 'r', 's' }},
+        {'8', new char[]{ 't', 'u', 'v' }},
+        {'9', new char[]{ 'w', 'x', 'y', 'z' }}
+    };
+
+    private string _digits;
+    private readonly List<string> _combinations = new List<string>();
+
+    public IList<string> Get(string digits)
+    {
+        if(string.IsNullOrEmpty(digits))
+            return _combinations;
+        _digits = digits;
+        
+        Backtrack(0, new StringBuilder());
+    
+        return _combinations;
+    }
+
+    private void Backtrack(int index, StringBuilder sb)
+    {
+        if (sb.Length == _digits.Length)
+        {
+            _combinations.Add(sb.ToString());
+            return;
+        }
+
+        var possibleLetters = _phoneKeys[_digits[index]];
+        foreach (var letter in possibleLetters)
+        {
+            sb.Append(letter);
+            Backtrack(index + 1, sb);
+            sb.Remove(sb.Length - 1, 1);
+        }
+    }
+}
