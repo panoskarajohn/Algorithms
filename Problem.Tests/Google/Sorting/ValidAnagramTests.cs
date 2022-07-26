b3eea795c1b9d5ba2edeaf2f6441da3718889c8e
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-26 16:42:53 +0300

    ValidAnagram

---
+++ Problem.Tests/Google/Sorting/ValidAnagramTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Sorting;
+
+namespace Problem.Tests.Google.Sorting;
+
+public class ValidAnagramTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "anagram",
+                    "nagaram",
+                    true
+                },
+                new object[]
+                {
+                    "rat",
+                    "car",
+                    false
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Valid_Anagram_Tests(string s, string t, bool expected)
+    {
+        var result = new ValidAnagram().IsAnagram(s, t);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/Sorting/ValidAnagram.cs
@@ -0,0 +1,13 @@
+﻿namespace Problems.Google.Sorting;
+
+public class ValidAnagram
+{
+    public bool IsAnagram(string s, string t)
+    {
+        var sChar = s.ToCharArray();
+        var tChar = t.ToCharArray();
+        System.Array.Sort(sChar);
+        System.Array.Sort(tChar);
+        return sChar.SequenceEqual(tChar);
+    }
+}
