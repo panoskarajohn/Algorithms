7cdf88610a411f864fb17ab13ed2463a23639bc6
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-26 17:03:26 +0300

    First and last occurence

---
+++ Problem.Tests/Google/Sorting/FirstAndLastElementTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Sorting;
+
+namespace Problem.Tests.Google.Sorting;
+
+public class FirstAndLastElementTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[]{5, 7, 7, 8, 8, 10},
+                    8,
+                    new[] {3, 4}
+                },
+                new object[]
+                {
+                    new int[]{5, 7, 7, 8, 8, 10},
+                    6,
+                    new[] {-1, -1}
+                },
+                new object[]
+                {
+                    new int[] { },
+                    0,
+                    new[] {-1, -1}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void First_And_Last_Element_Tests(int[] nums, int target, int[] expected)
+    {
+        var result = new FirstAndLastElement().Find(nums, target);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Google/Sorting/FirstAndLastElement.cs
@@ -0,0 +1,55 @@
+﻿namespace Problems.Google.Sorting;
+
+public class FirstAndLastElement
+{
+    public int[] Find(int[] nums, int target)
+    {
+        int first = BinarySearch(nums, target, false);
+
+        if (first == -1)
+            return new[] {-1, -1};
+
+        int second = BinarySearch(nums, target, true);
+        return new int[] {first, second};
+    }
+
+    int BinarySearch(int[] nums, int target, bool isOccured)
+    {
+        int n = nums.Length;
+        int begin = 0;
+        int end = n - 1;
+
+        while (begin <= end)
+        {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target)
+            {
+                if (!isOccured)
+                {
+                    if (mid == begin || nums[mid -1] != target)
+                        return mid;
+                    end = mid - 1;
+                }
+                else
+                {
+                    if (mid == end || nums[mid + 1] != target)
+                    {
+                        return mid;
+                    }
+
+                    begin = mid + 1;
+                }
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                begin = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
