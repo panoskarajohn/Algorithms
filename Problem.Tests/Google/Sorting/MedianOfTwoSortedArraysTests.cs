708387467f96340eeb4e3d8b8130cc4e8a285941
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-26 16:00:12 +0300

    Median of two sorted arrays

---
+++ Problem.Tests/Google/Sorting/MedianOfTwoSortedArraysTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Sorting;
+
+namespace Problem.Tests.Google.Sorting;
+
+public class MedianOfTwoSortedArraysTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 3},
+                    new[] {2},
+                    2.0000
+                },
+                new object[]
+                {
+                    new[] {1, 2},
+                    new[] {3, 4},
+                    2.50000
+                },
+                new object[]
+                {
+                    new[] {1, 2, 9, 10},
+                    new[] {-1, 0, 0, 2},
+                    1.50000
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Median_of_two_sorted_arrays_tests(int[] num1, int[] num2, double expected)
+    {
+        var result = new MedianOfTwoSortedArrays().FindMedian(num1, num2);
+        result.Should().BeApproximately(expected, 0.10);
+    }
+}
---
+++ Problems/Google/Sorting/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,69 @@
+﻿namespace Problems.Google.Sorting;
+
+public class MedianOfTwoSortedArrays
+{
+    /// <summary>
+    /// We want to do it efficiently
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public double FindMedian(int[] num1, int[] num2)
+    {
+        //Swap the arrays
+        if (num2.Length < num1.Length)
+            (num1, num2) = (num2, num1);
+
+        var low = 0;
+        var high = num1.Length;
+
+        int combined = num1.Length + num2.Length;
+
+        while (low <= high)
+        {
+            int partX = (low + high) / 2;
+            int partY = (combined + 1) / 2 - partX;
+
+            int leftX = GetMax(num1, partX);
+            int rightX = GetMin(num1, partX);
+            
+            int leftY = GetMax(num2, partY);
+            int rightY = GetMin(num2, partY);
+
+            if (leftX <= rightY && leftY <= rightX)
+            {
+                if (combined % 2 == 0)
+                    return (Math.Max(leftX, leftY) + Math.Min(rightX, rightY)) / 2.0;
+
+                return Math.Max(leftX, leftY);
+            }
+
+            if (leftX > rightY)
+            {
+                high = partX - 1;
+            }
+            else
+            {
+                low = partX + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private int GetMin(int[] nums, int partition)
+    {
+        if (partition == nums.Length)
+            return int.MaxValue;
+
+        return nums[partition];
+    }
+
+    private int GetMax(int[] nums, int partition)
+    {
+        if (partition == 0)
+            return int.MinValue;
+        
+        return nums[partition - 1];
+    }
+}
