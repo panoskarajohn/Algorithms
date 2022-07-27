f66ff8bd93c88589b5869962e3f61f59297878cf
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-28 00:24:33 +0300

    Max subarray

---
+++ Problem.Tests/Google/DP/MaximumSubArrayTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.DP;
+
+namespace Problem.Tests.Google.DP;
+
+public class MaximumSubArrayTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {-2, 1, -3, 4, -1, 2, 1, -5, 4},
+                    6
+                },
+                new object[]
+                {
+                    new[] {1},
+                    1
+                },
+                new object[]
+                {
+                    new[] {5, 4, -1, 7, 8},
+                    23
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Maximum_sub_array_tests(int[] nums, int expected)
+    {
+        var result = new MaximumSubarray().MaxSubArray(nums);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/DP/MaximumSubarray.cs
@@ -0,0 +1,28 @@
+﻿namespace Problems.Google.DP;
+
+public class MaximumSubarray
+{
+    /// <summary>
+    /// Dynamic programming Kadanes algorithm
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaxSubArray(int[] nums)
+    {
+        // Initialize our variables using the first element
+        var currentSubarray = nums[0];
+        var maxSubarray = nums[0];
+        
+        //start with the second element since we already used the first one
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            //if current subarray is negative, throw it away. Otherwise, keep adding it.
+
+            currentSubarray = Math.Max(num, currentSubarray + num);
+            maxSubarray = Math.Max(maxSubarray, currentSubarray);
+        }
+        
+        return maxSubarray;
+    }
+}
