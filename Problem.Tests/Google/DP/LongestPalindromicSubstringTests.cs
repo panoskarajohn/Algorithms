a32b5b02fda6d2a9f98ae5f686368d7069f48e33
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-27 20:09:18 +0300

    Longest Palindromic substring

---
+++ Problem.Tests/Google/DP/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.DP;
+
+namespace Problem.Tests.Google.DP;
+
+public class LongestPalindromicSubstringTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Longest_Palindromic_Substring_Tests(string s, string expected)
+    {
+        var result = new LongestPalindromicSubstring().Get(s);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "babad", 
+                    "aba"
+                },
+                new object[]
+                {
+                    "cbbd", 
+                    "bb"
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Google/DP/LongestPalindromicSubstring.cs
@@ -0,0 +1,39 @@
+﻿namespace Problems.Google.DP;
+
+public class LongestPalindromicSubstring
+{
+    public string Get(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return string.Empty;
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int lengthOne = Expand(s, i, i);
+            int lengthTwo = Expand(s, i, i + 1);
+            int maxLength = Math.Max(lengthOne, lengthTwo);
+
+            if (maxLength > end - start)
+            {
+                start = i - (maxLength - 1) / 2;
+                end = i + maxLength / 2;
+            }
+        }
+
+        return s[start..(end+1)];
+    }
+
+    private int Expand(string s, int left, int right)
+    {
+        while (left >= 0 && right < s.Length && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
