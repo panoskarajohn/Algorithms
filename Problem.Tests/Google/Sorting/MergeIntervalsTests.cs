af26753f8bae3050a40f2ae728fc11d7577eb71c
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-27 17:39:59 +0300

    Merge Intervals

---
+++ Problem.Tests/Google/Sorting/MergeIntervalsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Sorting;
+
+namespace Problem.Tests.Google.Sorting;
+
+public class MergeIntervalsTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {0, 4}, new[] {3, 5}},
+                    new[] {new[] {0, 5}}
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 4}, new[] {2, 3}},
+                    new[] {new[] {1, 4}}
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 3}, new[] {2, 6}, new[] {8, 10}, new[] {15, 18}},
+                    new[] {new[] {1, 6}, new[] {8, 10}, new[] {15, 18}}
+                },
+                new object[]
+                {
+                    new[] {new[] {1, 4}, new[] {0, 2}, new[] {3, 5}},
+                    new[] {new[] {0, 5}}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Merge_Intervals_Tests(int[][] intervals, int[][] expected)
+    {
+        var result = new MergeIntervals().Merge(intervals);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Google/Sorting/MergeIntervals.cs
@@ -0,0 +1,50 @@
+﻿namespace Problems.Google.Sorting;
+
+public class MergeIntervals
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        var result = new Stack<int[]>();
+
+        if (intervals is null)
+            return result.ToArray();
+
+        if (intervals.Length == 1)
+        {
+            result.Push(intervals[0]);
+            return result.ToArray();
+        }
+
+        var comparer = Comparer<int>.Default;
+        //array based on the first value
+        System.Array.Sort(intervals, (a, b)
+            => comparer.Compare(a[0], b[0]));
+
+        result.Push(intervals[0]);
+        for (var i = 1; i < intervals.Length; i++)
+        {
+            var current = result.Peek();
+            var next = intervals[i];
+
+            var nextZeroIsInRangeFromNext = current[1] >= next[0] && current[1] <= next[1];
+            var nextIsLargerThanCurrent = current[1] >= next[0] && current[1] >= next[1];
+
+            if (nextZeroIsInRangeFromNext)
+            {
+                result.Pop();
+                result.Push(new[] {current[0], next[1]});
+            }
+            else if (nextIsLargerThanCurrent)
+            {
+                result.Pop();
+                result.Push(new[] {current[0], current[1]});
+            }
+            else
+            {
+                result.Push(next);
+            }
+        }
+
+        return result.ToArray().Reverse().ToArray();
+    }
+}
