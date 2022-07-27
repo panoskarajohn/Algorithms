cd40d5488d7588ffec541cf66e586237c422f117
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-27 17:39:42 +0300

    Insert intervals

---
+++ Problem.Tests/Google/Sorting/InsertIntervalTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.Sorting;
+
+namespace Problem.Tests.Google.Sorting;
+
+public class InsertIntervalTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {1, 3}, new[] {6, 9}},
+                    new[] {2, 5},
+                    new[] {new[] {1, 5}, new[] {6, 9}}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Insert_Interval_Tests(int[][] intervals, int[] intervalToAdd, int[][] expected)
+    {
+        var result = new InsertInterval().Insert(intervals, intervalToAdd);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Google/Sorting/InsertInterval.cs
@@ -0,0 +1,60 @@
+﻿namespace Problems.Google.Sorting;
+
+public class InsertInterval
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+       //define list
+       var stack = new Stack<int[]>();
+       int i = 0;
+       int n = intervals.Length;
+
+       //insert the new interval
+       while (i <= n)
+       {
+           if (i == n || newInterval[0] <= intervals[i][0])
+           {
+               if (!stack.Any())
+               {
+                   stack.Push(newInterval);
+               }
+               else if (newInterval[0] <= stack.Peek()[1])
+               {
+                   //prev interval is in conflict with new interval
+                   stack.Peek()[1] = Math.Max(stack.Peek()[1], newInterval[1]);
+               }
+               else
+               {
+                   stack.Push(newInterval);
+               }
+
+               break;
+           }
+
+           stack.Push(intervals[i]);
+           i++;
+       }
+       
+       //add the remaining
+       for (int j = 0; j < n; j++)
+       {
+           int[] prev = stack.Peek();
+           if (intervals[j][0] <= prev[1])
+           {
+               prev[1] = Math.Max(intervals[j][1], prev[1]);
+           }
+           else
+           {
+               stack.Push(intervals[j]);
+           }
+       }
+
+       return stack
+           .ToArray()
+           .Reverse()
+           .ToArray();
+
+
+    }
+    
+}
