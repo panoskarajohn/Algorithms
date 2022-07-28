609eb73d3a940bb3ea5f52390321775de0253344
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-28 17:37:14 +0300

    PlatesBetweenCandles

---
+++ Problem.Tests/Amazon/PlatesBetweenCandlesTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Amazon;
+
+namespace Problem.Tests.Amazon;
+
+public class PlatesBetweenCandlesTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new Object[]
+                {
+                    "***|**|*****|**||**|*", 
+                    new int[][]
+                    {
+                        new int[] {1,17},
+                        new int[] {4,5},
+                        new int[] {14,17},
+                        new int[] {5,11},
+                        new int[] {15,16},
+                    },
+                    new int[] {9,0,0,0,0}
+                },
+                new Object[]
+                {
+                    "**|**|***|", 
+                    new int[][]
+                    {
+                        new int[] {2,5},
+                        new int[] {5,9},
+                    },
+                    new int[] {2,3}
+                },
+                
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Plates_Between_Candles_Tests(string s, int[][] queries, int[] expected)
+    {
+        var result = new PlatesBetweenCandles().Count(s, queries);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Amazon/PlatesBetweenCandles.cs
@@ -0,0 +1,64 @@
+﻿namespace Problems.Amazon;
+
+public class PlatesBetweenCandles
+{
+    /// <summary>
+    /// Using prefix sum
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="queries"></param>
+    /// <returns></returns>
+    public int[] Count(string s, int[][] queries)
+    {
+        /*
+         * Brainstorming
+         * Example:
+         *  0 1 2 3 4 5 6 7 8 9
+         *  * * | * | * * * * |
+         *  1 2 2 3 3 4 5 6 7 7 <- Prefix sum
+         */
+
+
+        int n = s.Length;
+
+        int[] prefixSum = new int[n];
+        int[] candleLeft = new int[n];
+        int[] candleRight = new int[n];
+
+        prefixSum[0] = s[0] == '*' ? 1 : 0;
+        candleLeft[0] = s[0] == '|' ? 0 : -1;
+
+        for (int i = 1; i < n; i++)
+        {
+            prefixSum[i] = prefixSum[i - 1] + (s[i] == '*' ? 1 : 0);
+            candleLeft[i] = s[i] == '|' ? i : candleLeft[i - 1];
+        }
+        
+        candleRight[n - 1] = s[n - 1] == '|' ? n - 1 : n;
+
+        for (int i = n - 2; i >= 0 ; i--)
+        {
+            candleRight[i] = s[i] == '|' ? i : candleRight[i + 1];
+        }
+
+        int[] result = new int[queries.Length];
+        for (int i = 0; i < queries.Length; i++)
+        {
+            /*
+             * 0 1 2 3 4 5 6 7 8 9
+             * * * | * | * * * * |
+             * 1 2 2 3 3 4 5 6 7 8 <- prefix sum
+             * There is a special case when we get 5 - 8
+             * which should return 0
+             * In that case our start will be 9 and end will be 4
+             * 
+             */
+            int start = candleRight[queries[i][0]];
+            int end = candleLeft[queries[i][1]];
+
+            result[i] = start > end ? 0 : prefixSum[end] - prefixSum[start];
+        }
+
+        return result;
+    }
+}
