d81a152c95921799f34701278d0603436c626837
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-28 13:15:56 +0300

    BestTimeToBuyAndSellStock.cs

---
+++ Problem.Tests/Google/DP/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Problems.Google.DP;
+
+namespace Problem.Tests.Google.DP;
+
+public class BestTimeToBuyAndSellStockTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {7, 1, 5, 3, 6, 4},
+                    5
+                },
+                new object[]
+                {
+                    new[] { 7,6,4,3,1 },
+                    5
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Best_Time_To_Sell_And_Buy_Stock(int[] prices, int expected)
+    {
+        var result = new BestTimeToBuyAndSellStock().MaxProfit(prices);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Google/DP/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Google.DP;
+
+public class BestTimeToBuyAndSellStock
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0;
+        int min = int.MaxValue;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < min)
+            {
+                min = prices[i];
+            }
+            else
+            {
+                max = Math.Max(max, prices[i] - min);
+            }
+        }
+        
+        return max;
+    }
+}
