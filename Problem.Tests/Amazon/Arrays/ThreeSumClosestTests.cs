03b61334036400913a4c6038f58c3adf2d7b7228
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-29 17:11:24 +0300

    Three sum closest

---
+++ Problem.Tests/Amazon/Arrays/ThreeSumClosestTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Problems.Amazon.Arrays;
+
+namespace Problem.Tests.Amazon.Arrays;
+
+
+public class ThreeSumClosestTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Three_sum_closest_tests(int[] data, int target, int expected)
+    {
+        var result = new ThreeSumClosest().Closest(data, target);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { -1, 2,1,-4 }, 
+                    1,
+                    2
+                },
+                new object[]
+                {
+                    new int[] { 0,0,0 }, 
+                    1,
+                    0
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Amazon/Arrays/ThreeSumClosest.cs
@@ -0,0 +1,44 @@
+﻿namespace Problems.Amazon.Arrays;
+
+public class ThreeSumClosest
+{
+    public int Closest(int[] nums, int target)
+    {
+        int diff = int.MaxValue;
+        
+        if (nums.Length < 3)
+            return 0;
+        
+        System.Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(diff == 0)
+                break;
+            
+            var current = nums[i];
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = current + nums[left] + nums[right];
+                if (Math.Abs(target - sum) < Math.Abs(diff))
+                    diff = target - sum;
+
+                if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+                
+            }
+
+            
+        }
+        return target - diff;
+    }
+}
