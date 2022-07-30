bd4e5c4fe1b1980421b9c688a37798ce292e5e6b
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-30 14:27:21 +0300

    Number of dice rolls WTF!!

---
+++ Problem.Tests/Amazon/Recursion/NumberOfDiceRollsToTargetTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Problems.Amazon.Recursion;
+
+namespace Problem.Tests.Amazon.Recursion;
+
+public class NumberOfDiceRollsToTargetTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Number_of_dice_rolls_to_target_tests(int n, int k, int target, int expected)
+    {
+        var result = new NumberOfDiceRolls().NumRollsToTarget(n, k, target);
+        result.Should().Be(expected);
+
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    1, 6, 3, 1
+                },
+                new object[]
+                {
+                    2, 6, 7, 6
+                },
+                new object[]
+                {
+                    30, 30, 500, 222616187
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Amazon/Recursion/NumberOfDiceRolls.cs
@@ -0,0 +1,47 @@
+﻿using Problems.Google.Recursion;
+
+namespace Problems.Amazon.Recursion;
+
+public class NumberOfDiceRolls
+{
+    private const int MODULO = 1_000_000_007;
+    
+    public int NumRollsToTarget(int n, int k, int target)
+    {
+        int[,] memo = new int[n + 1,target + 1];
+        Fill(memo);
+        var res = WaysToTarget(memo, 0, n, 0, target, k);
+        return res;
+    }
+
+    private void Fill(int[,] memo, int value = -1)
+    {
+        for (int i = 0; i < memo.GetLength(0); i++)
+        {
+            for (int j = 0; j < memo.GetLength(1); j++)
+            {
+                memo[i, j] = value;
+            }
+        }
+    }
+
+    private int WaysToTarget(int[,] memo, int diceIndex, int n, int currentSum, int target, int k)
+    {
+        // all dices are visited
+        if (diceIndex == n)
+            return currentSum == target ? 1 : 0;
+
+        if (memo[diceIndex, currentSum] != -1)
+            return memo[diceIndex, currentSum];
+
+        int ways = 0;
+        for (int i = 1; i <= Math.Min(k, target - currentSum); i++)
+        {
+            ways = (ways + 
+                    WaysToTarget(memo, diceIndex + 1, n, currentSum + i, target, k)) % MODULO;
+        }
+
+        memo[diceIndex, currentSum] = ways;
+        return ways;
+    }
+}
