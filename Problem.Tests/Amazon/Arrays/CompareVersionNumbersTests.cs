daef32549876f6fe47dd9e9e6c60984e94b4448b
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-31 12:36:09 +0300

    compare version numbers

---
+++ Problem.Tests/Amazon/Arrays/CompareVersionNumbersTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Problems.Amazon.Arrays;
+
+namespace Problem.Tests.Amazon.Arrays;
+
+public class CompareVersionNumbersTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Compare_version_numbers(string v1, string v2, int expected)
+    {
+        var result = new CompareVersionNumbers().CompareVersion(v1, v2);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "1.01",
+                    "1.001",
+                    0
+                },
+                new object[]
+                {
+                    "1.0",
+                    "1.0.0",
+                    0
+                },
+                new object[]
+                {
+                    "0.1",
+                    "1.1",
+                    -1
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Amazon/Arrays/CompareVersionNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace Problems.Amazon.Arrays;
+
+public class CompareVersionNumbers
+{
+    public int CompareVersion(string version1, string version2)
+    {
+        var version1Split = version1.Split('.');
+        var version2Split = version2.Split('.');
+
+        int n = Math.Max(version1Split.Length, version2Split.Length);
+        
+        for (int i = 0; i < n; i++)
+        {
+            int group1 = (i < version1Split.Length) ? GetGroupVersion(version1Split[i]) : 0;
+            int group2 = (i < version2Split.Length) ? GetGroupVersion(version2Split[i]) : 0;
+
+            if (group1 < group2)
+                return -1;
+            if (group1 > group2)
+                return 1;
+        }
+        
+        return 0;
+    }
+
+    private int GetGroupVersion(string group)
+    {
+        int sum = 0;
+        int indexOfNonZero = 0;
+
+        while (indexOfNonZero < group.Length && group[indexOfNonZero] == '0')
+            indexOfNonZero++;
+
+        for (int i = indexOfNonZero; i < group.Length; i++)
+        {
+            var current = group[i] - '0';
+            sum = (sum * 10) + current;
+        }
+        
+        return sum;
+    }
+}
