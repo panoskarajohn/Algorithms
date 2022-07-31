9f431e7201ecfae14c85efc21c23e29ea2bf957e
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-31 15:21:06 +0300

    Missing numbers

---
+++ Problems/Amazon/Arrays/MissingNumbers.cs
@@ -0,0 +1,18 @@
+﻿using Problems.DataStructures;
+
+namespace Problems.Amazon.Arrays;
+
+public class MissingNumbers
+{
+    public int Find(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 1; i < nums.Length + 1; i++)
+            sum += i;
+
+        var currentSum = nums.Sum();
+
+        return sum - currentSum;
+
+    }
+}
