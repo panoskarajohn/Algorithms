c06873590e6d3fa49c5e9b799f24f40fc72b624e
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-31 13:06:21 +0300

    Product array

---
+++ Problem.Tests/Amazon/Arrays/ProductArrayExceptSelfTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Problems.Amazon.Arrays;
+
+namespace Problem.Tests.Amazon.Arrays;
+
+
+public class ProductArrayExceptSelfTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Product_array_except_self_tests(int[] data, int[] expected)
+    {
+        var res = new ProductArray().GetExceptSelf(data);
+        res.Should().BeEquivalentTo(expected);
+    }
+    
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Product_array_except_self_Optimized_tests(int[] data, int[] expected)
+    {
+        var res = new ProductArray().GetExceptSelfOptimized(data);
+        res.Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 1,2,3,4 },
+                    new int[] { 24,12,8,6 }
+                },
+                new object[]
+                {
+                    new int[] { -1, 1, 0, -3, 3 },
+                    new int[] { 0,0,9,0,0 }
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Amazon/Arrays/ProductArray.cs
@@ -0,0 +1,69 @@
+﻿namespace Problems.Amazon.Arrays;
+
+public class ProductArray
+{
+    public int[] GetExceptSelf(int[] values)
+    {
+        var n = values.Length;
+
+        var prefixProduct = new int[n];
+        var suffixProduct = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            int previousIndex = i - 1;
+            var prefix = (previousIndex < 0) ? 1 : prefixProduct[i - 1];
+            var previous = (previousIndex < 0) ? 1 : values[i - 1];
+
+            prefixProduct[i] = prefix * previous;
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int nextIndex = i + 1;
+            var suffix = (nextIndex >= n) ? 1 : suffixProduct[i + 1];
+            var next = (nextIndex >= n) ? 1 : values[i + 1];
+
+            suffixProduct[i] = suffix * next;
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = prefixProduct[i] * suffixProduct[i];
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// O(1) space
+    /// result array does not count
+    /// </summary>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    public int[] GetExceptSelfOptimized(int[] values)
+    {
+        var n = values.Length;
+        Span<int> result = stackalloc int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            int previousIndex = i - 1;
+            var prefix = (previousIndex < 0) ? 1 : result[previousIndex];
+            var previous = (previousIndex < 0) ? 1 : values[previousIndex];
+
+            result[i] = prefix * previous;
+        }
+
+        var right = 1;
+        for (var i = n - 1; i >= 0; i--)
+        {
+            result[i] *= right;
+            right *= values[i];
+        }
+
+        return result.ToArray();
+    }
+}
