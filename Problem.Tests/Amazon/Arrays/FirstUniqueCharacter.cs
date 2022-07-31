a3399f4a1b4689b64e89ea6f9cbfe0634023c4ac
panoskarajohn <panoskarajohn@hotmail.com>
2022-07-31 17:32:24 +0300

    First unique character

---
+++ Problem.Tests/Amazon/Arrays/FirstUniqueCharacter.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using FluentAssertions;
+
+namespace Problem.Tests.Amazon.Arrays;
+
+public class FirstUniqueCharacter
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void First_unique_character(string s, int expected)
+    {
+        var result = new FirstUniqueChar().GetFirstIndex(s);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "leetcode",
+                    0
+                },
+                new object[]
+                {
+                    "loveleetcode",
+                    2
+                },
+                new object[]
+                {
+                    "aabb",
+                    -1
+                },
+                
+            };
+        }
+    }
+}
+
+public class FirstUniqueChar
+{
+    public int GetFirstIndex(string s)
+    {
+        var map = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var current = s[i];
+            if (!map.ContainsKey(current))
+                map[current] = 0;
+            map[current]++;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var current = s[i];
+            if (map[current] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+}
