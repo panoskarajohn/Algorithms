0d729a5be3c35bda1e799d633e280d13f6161868
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-07 00:31:20 +0300

    Merge similar items

---
+++ Problem.Tests/Contest/MergeSimilarItemTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Problems.Contests;
+
+namespace Problem.Tests.Contest;
+
+public class MergeSimilarItemTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Merge_similar_item_tests(int[][] items1, int[][] items2, IList<IList<int>> expexted)
+    {
+        var result = new MergeSimilarItems().Merge(items1, items2);
+        result.Should().BeEquivalentTo(expexted);
+
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[][] { new []{1,1}, new []{4,5}, new []{3,8} },
+                    new int[][] { new []{3,1}, new []{1,5}},
+                    new List<IList<int>>
+                    {
+                        new List<int>() { 1,6 },
+                        new List<int>() { 3,9 },
+                        new List<int>() { 4,5 }
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Contests/MergeSimilarItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+
+namespace Problems.Contests;
+
+public class MergeSimilarItems
+{
+    public IList<IList<int>> Merge(int[][] items1, int[][] items2)
+    {
+        var dic = new Dictionary<int, int>();
+        foreach (var i in items1)
+        {
+            dic.Add(i[0], i[1]);
+        }
+
+        foreach (var i in items2)
+        {
+            if (dic.ContainsKey(i[0]))
+                dic[i[0]] += i[1];
+            else
+            {
+                dic.Add(i[0], i[1]);
+            }
+
+        }
+
+        var result = new List<IList<int>>();
+        foreach (var i in dic)
+        {
+            result.Add(new List<int>() { i.Key, i.Value });
+        }
+
+        var comparer = Comparer<int>.Default;
+        result.Sort((a, b) => comparer.Compare(a[0], b[0]));
+        return result;
+    }
+}
