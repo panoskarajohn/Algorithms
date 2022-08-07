205c8f16d2920a3ab1fa939e02c6e9744df14ca1
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-07 17:42:56 +0300

    House Robber

---
+++ Problem.Tests/DP/HouseRobberTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class HouseRobberTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void House_robber_tests(int[] data, int expected)
+    {
+        var result = new HouseRobber().Rob(data);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 1,2,3,1 },
+                    4
+                },
+                new object[]
+                {
+                    new int[] { 2,7,9,3,1 },
+                    12
+                },
+            };
+        }
+    }
+}
