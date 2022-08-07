3c5f16af5da867368cf31e1cf0d5a6bc885a48b9
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-07 23:25:33 +0300

    Min Cost climbing stairs

---
+++ Problem.Tests/DP/MinCostClimbingStairsTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class MinCostClimbingStairsTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Min_cost_climbing_stairs_tests(int[] data, int expected)
+    {
+        var result = new MinCostClimbingStairs().Get(data);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 10,15,20 },
+                    15
+                },
+                new object[]
+                {
+                    new int[] { 1,100,1,1,1,100,1,1,100,1 },
+                    6
+                },
+            };
+        }
+    }
+}
---
+++ Problems/DP/MinCostClimbingStairs.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.DP;
+
+public class MinCostClimbingStairs
+{
+    public int Get(int[] cost)
+    {
+        int n = cost.Length;
+        if (n == 1)
+            return cost[0];
+        if (n == 2)
+            return Math.Min(cost[0], cost[1]);
+
+        var dp = new int[n + 1];
+        dp[n] = 0;
+        dp[n - 1] = Math.Min(cost[n - 1], cost[n - 2]);
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            dp[i] = cost[i] + Math.Min(dp[i + 1], dp[i + 2]);
+        }
+
+        return Math.Min(dp[0], dp[1]);
+    }
+}
