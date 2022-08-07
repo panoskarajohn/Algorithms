e0443b951a0d89405dafd8436ee3738d67e64e21
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-07 23:53:34 +0300

    NthTribonacciNumber

---
+++ Problem.Tests/DP/NthTribonacciNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+
+public class NthTribonacciNumberTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Nth_tribonacci_number_tests(int n, int expected)
+    {
+        var result = new NthTribonacciNumbers().Get(n);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    4,
+                    4
+                },
+                new object[]
+                {
+                    25,
+                    1_389_537
+                },
+            };
+        }
+    }
+}
