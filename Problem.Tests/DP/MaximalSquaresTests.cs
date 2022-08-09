58b212639ff2e2e9ac3bfc9e77e827d6dce11351
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-09 16:18:56 +0300

    Maximal square

---
+++ Problem.Tests/DP/MaximalSquaresTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class MaximalSquaresTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] {'1', '0', '1', '0', '0'}, new[] {'1', '0', '1', '1', '1'},
+                        new[] {'1', '1', '1', '1', '1'}, new[] {'1', '0', '0', '1', '0'}
+                    },
+                    4
+                },
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] { '0', '1'}, new[] {'1', '0'}
+                    },
+                    1
+                },
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] { '0'}
+                    },
+                    0
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Maximal_squares_tests(char[][] matrix, int expected)
+    {
+        var result = new MaximalSquare().Get(matrix);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/MaximalSquare.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems.DP;
+
+public class MaximalSquare
+{
+    public int Get(char[][] matrix)
+    {
+        int n = matrix.Length;
+        int m = matrix[0].Length;
+
+        int[][] dp = new int[n + 1][];
+        for (int i = 0; i < dp.Length; i++)
+        {
+            dp[i] = new int[m + 1];
+        }
+
+        var max = 0;
+
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    dp[i][j] = 1 + Math.Min(dp[i + 1][j], 
+                        Math.Min(dp[i + 1][j + 1] , dp[i][j + 1]));
+                }
+
+                max = Math.Max(max, dp[i][j]);
+            }
+        }
+
+        return max * max;
+    }
+}
