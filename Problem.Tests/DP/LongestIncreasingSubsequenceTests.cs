97c9b974054d0199d1f8bcc1852b9daf87ca2625
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-12 00:02:23 +0300

    Longest increasing subsequence

---
+++ Problem.Tests/DP/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Problem.Tests.Google.DP;
+
+namespace Problem.Tests.DP;
+
+public class LongestIncreasingSubsequenceTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {10, 9, 2, 5, 3, 7, 101, 18},
+                    4
+                },
+                new object[]
+                {
+                    new[] {0, 1, 0, 3, 2, 3},
+                    4
+                },
+                new object[]
+                {
+                    new[] {1, 2, 4, 3},
+                    3
+                },
+                new object[]
+                {
+                    new[] {7, 7, 7, 7, 7, 7, 7},
+                    1
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Longest_increasing_subsequence_tests(int[] data, int expected)
+    {
+        var result = new LongestIncreasingSubsequence().Get(data);
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Longest_increasing_subsequence_BS_tests(int[] data, int expected)
+    {
+        var result = new LongestIncreasingSubsequence().GetOpt(data);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problem.Tests/Google/DP/LongestIncreasingSubsequence.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace Problem.Tests.Google.DP;
+
+public class LongestIncreasingSubsequence
+{
+    /// <summary>
+    ///     O(n ^ 2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int Get(int[] nums)
+    {
+        var n = nums.Length;
+
+        var lis = new int[n];
+        System.Array.Fill(lis, 1);
+        var max = 1;
+
+        for (var i = n - 1; i >= 0; i--)
+        for (var j = i + 1; j < n; j++)
+            if (nums[i] < nums[j])
+            {
+                lis[i] = Math.Max(lis[i], 1 + lis[j]);
+                max = Math.Max(max, lis[i]);
+            }
+
+
+        return max;
+    }
+
+    /// <summary>
+    ///     O(n log(n))
+    ///     Improve with binary search
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int GetOpt(int[] nums)
+    {
+        int n = nums.Length;
+        var subSequence = new List<int>();
+        subSequence.Add(nums[0]);
+
+        for (int i = 1; i < n; i++)
+        {
+            int num = nums[i];
+            if (num > subSequence[^1])
+            {
+                subSequence.Add(num);
+            }
+            else
+            {
+                int getRight = BinarySearch(subSequence, num);
+                subSequence[getRight] = num;
+            }
+        }
+
+        return subSequence.Count;
+    }
+
+    private int BinarySearch(List<int> subSequence, int num)
+    {
+        var left = 0;
+        var right = subSequence.Count - 1;
+        int mid;
+
+        while (left < right)
+        {
+            mid = (left + right) / 2;
+            
+            if (subSequence[mid] == num)
+                return mid;
+
+            if (subSequence[mid] < num)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+}
