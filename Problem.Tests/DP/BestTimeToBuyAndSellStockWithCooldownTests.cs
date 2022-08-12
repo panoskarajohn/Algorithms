89915a20fa1794a94efbfecf0e295d9cfb02c1e8
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-12 23:13:28 +0300

    Best time to buy and sell stock with cooldown

---
+++ Problem.Tests/DP/BestTimeToBuyAndSellStockWithCooldownTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class BestTimeToBuyAndSellStockWithCooldownTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 2, 3, 0, 2},
+                    3
+                },
+                new object[]
+                {
+                    new[] {1},
+                    0
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Best_time_to_buy_and_sell_stock_with_cooldown_tests(int[] data, int expected)
+    {
+        var result = new BestTimeToBuyAndSellStockWithCooldown().MaxProfitWithCooldown(data);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Best_time_to_buy_and_sell_stock_with_cooldown_opt_tests(int[] data, int expected)
+    {
+        var result = new BestTimeToBuyAndSellStockWithCooldown().MaxProfitWithCooldownOpt(data);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/BestTimeToBuyAndSellStockWithCooldown.cs
@@ -0,0 +1,51 @@
+﻿namespace Problems.DP;
+
+public class BestTimeToBuyAndSellStockWithCooldown
+{
+    private readonly Dictionary<(int, bool), int> _cache = new();
+
+    public int MaxProfitWithCooldown(int[] prices)
+    {
+        return Dfs(prices, 0, true);
+    }
+
+    private int Dfs(in int[] prices, int i, bool buying)
+    {
+        if (i >= prices.Length)
+            return 0;
+
+        if (_cache.ContainsKey((i, buying)))
+            return _cache[(i, buying)];
+
+        var cooldown = Dfs(prices, i + 1, true);
+
+        if (buying)
+        {
+            var buy = -prices[i] + Dfs(prices, i + 1, false);
+            _cache[(i, buying)] = Math.Max(buy, cooldown);
+        }
+        else
+        {
+            var sell = prices[i] + Dfs(prices, i + 2, true);
+            _cache[(i, buying)] = Math.Max(sell, cooldown);
+        }
+
+        return _cache[(i, buying)];
+    }
+
+    public int MaxProfitWithCooldownOpt(int[] prices)
+    {
+        int sold = int.MinValue, held = int.MinValue, reset = 0;
+
+        foreach (var price in prices)
+        {
+            var preSold = sold;
+
+            sold = held + price;
+            held = Math.Max(held, reset - price);
+            reset = Math.Max(reset, preSold);
+        }
+
+        return Math.Max(sold, reset);
+    }
+}
