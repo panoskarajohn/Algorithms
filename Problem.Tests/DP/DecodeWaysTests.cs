4869fe6710c3eb4fb393355c1d16853f529380a0
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-13 18:17:31 +0300

    Yeah baby Decode ways bottom up my Solution

---
+++ Problem.Tests/DP/DecodeWaysTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class DecodeWaysTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "12",
+                    2
+                },
+                new object[]
+                {
+                    "226",
+                    3
+                },
+                new object[]
+                {
+                    "06",
+                    0
+                },
+                new object[]
+                {
+                    "10",
+                    1
+                },
+                new object[]
+                {
+                    "27",
+                    1
+                },
+                new object[]
+                {
+                    "2101",
+                    1
+                },
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Decode_ways_tests(string s, int expected)
+    {
+        var result = new DecodeWays().NumDecodings(s);
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Decode_ways_Bottom_Up_tests(string s, int expected)
+    {
+        var result = new DecodeWays().NumWaysDecodingBottomUp(s);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/DecodeWays.cs
@@ -0,0 +1,78 @@
+﻿namespace Problems.DP;
+
+public class DecodeWays
+{
+    #region TopDown
+    private readonly Dictionary<int, int> _cache = new Dictionary<int, int>();
+    public int NumDecodings(string s)
+    {
+        return Dfs(0,  s);
+    }
+
+    int Dfs(int index, string s)
+    {
+        if (_cache.ContainsKey(index))
+            return _cache[index];
+
+        if (index == s.Length)
+            return 1;
+
+        if (s[index] == '0')
+            return 0;
+
+        if (index == s.Length - 1)
+            return 1;
+
+        int one = Dfs(index + 1, s);
+        var number = int.Parse(s[index..(index + 2)]);
+        if (number <= 26)
+        {
+            one += Dfs(index + 2, s);
+        }
+
+        _cache[index] = one;
+        return _cache[index];
+
+    }
+    
+    #endregion
+
+    public int NumWaysDecodingBottomUp(string s)
+    {
+        if (s.StartsWith('0'))
+            return 0;
+        int n = s.Length;
+        
+        var dp = new int[n];
+        dp[n - 1] = s[n - 1] == '0' ? 0 : 1;
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            var answer = dp[i + 1];
+            if (s[i] == '0')
+            {
+                dp[i] = 0;
+                continue;
+            }
+            int number = int.Parse(s[i..(i + 2)]);
+            if (number > 26)
+            {
+                dp[i] = answer;
+                continue;
+            }
+
+            if (i + 2 < n)
+            {
+                answer += dp[i + 2];
+            }
+            else
+            {
+                answer++;
+            }
+
+            dp[i] = answer;
+        }
+
+        return dp[0];
+    }
+}
