cc28ffc63b2aaba1315af14c3dab2cd224f88873
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-14 17:19:21 +0300

    Maximum circular subarray wtf problem

---
+++ Problem.Tests/DP/MaximumSumCircularSubarrayTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class MaximumSumCircularSubarrayTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, -2, 3, -2},
+                    3
+                },
+                new object[]
+                {
+                    new[] {5, -3, 5},
+                    10
+                },
+                new object[]
+                {
+                    new[] {-3, -2, -3},
+                    -2
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Maximum_sum_circular_subarray_tests(int[] data, int expected)
+    {
+        var result = new MaximumSubCircularSubArray().MaxSubarraySumCircular(data);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/MaximumSubCircularSubArray.cs
@@ -0,0 +1,32 @@
+﻿namespace Problems.DP;
+
+public class MaximumSubCircularSubArray
+{
+    /// <summary>
+    ///     Kadane's algorithm
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaxSubarraySumCircular(int[] nums)
+    {
+        var totalSum = 0;
+        var maxEndingAt = 0;
+        var minEndingAt = 0;
+        var minSum = int.MaxValue;
+        var maxSum = int.MinValue;
+
+        foreach (var num in nums)
+        {
+            totalSum += num;
+            maxEndingAt = Math.Max(maxEndingAt + num, num);
+            maxSum = Math.Max(maxEndingAt, maxSum);
+
+            minEndingAt = Math.Min(minEndingAt + num, num);
+            minSum = Math.Min(minEndingAt, minSum);
+        }
+
+        if (maxSum > 0) return Math.Max(maxSum, totalSum - minSum);
+
+        return maxSum;
+    }
+}
