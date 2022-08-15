96b23d8166e145da2830ac6baff70cc17d2e1fc4
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-15 23:49:35 +0300

    Paint house

---
+++ Problem.Tests/Series/PaintHouse/PaintHouseTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace Problem.Tests.Series.PaintHouse;
+
+public class PaintHouseTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Paint_house_tests(int[][] data, int expected)
+    {
+        var result = new Problems.Series.PaintHouse.PaintHouse().MinCost(data);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {17, 2, 17}, new[] {16, 16, 5}, new[] {14, 3, 19}},
+                    10
+                },
+                new object[]
+                {
+                    new[] {new[] {7, 6, 2}},
+                    2
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Series/PaintHouse/PaintHouse.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Series.PaintHouse;
+
+public class PaintHouse
+{
+    public int MinCost(int[][] costs)
+    {
+        if (costs is null || costs.Length == 0)
+            return 0;
+
+        int min = int.MinValue;
+        for (int i = 1; i < costs.Length; i++)
+        {
+            costs[i][0] += Math.Min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.Min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.Min(costs[i - 1][0], costs[i - 1][1]);
+        }
+
+        return costs[^1].Min();
+
+    }
+}
+
+
+
