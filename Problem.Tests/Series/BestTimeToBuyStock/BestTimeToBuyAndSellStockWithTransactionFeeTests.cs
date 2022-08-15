d77ec5a0d56a340b08ec7515647efb485950cb21
panoskarajohn <panoskarajohn@hotmail.com>
2022-08-15 20:53:38 +0300

    BestTimeToBuyAndSellStockWithTransactionFee.cs

--- Problem.Tests/Series/BestTimeToBuyStock/BestTimeToBuyAndSellStockWithTransactionFeeTests.cs
+++ Problem.Tests/Series/BestTimeToBuyStock/BestTimeToBuyAndSellStockWithTransactionFeeTests.cs
@@ -1,18 +1,10 @@
 ﻿using FluentAssertions;
-using Problems.DP;
 using Problems.Series.BestTimeToBuyStock;
 
 namespace Problem.Tests.Series.BestTimeToBuyStock;
 
 public class BestTimeToBuyAndSellStockWithTransactionFeeTests
 {
-    [Theory, MemberData(nameof(TestDataProperty))]
-    public void Best_time_to_buy_and_sell_stock_with_transaction_fee_tests(int[] prices, int fee, int expected)
-    {
-        var result = new BestTimeToBuyAndSellStockWithTransactionFee().MaxProfitWithFee(prices, fee);
-        result.Should().Be(expected);
-    }
-    
     public static IEnumerable<object[]> TestDataProperty
     {
         get
@@ -21,17 +13,33 @@ public class BestTimeToBuyAndSellStockWithTransactionFeeTests
             {
                 new object[]
                 {
-                    new int[] { 1,3,2,8,4,9 },
-                    2, 
+                    new[] {1, 3, 2, 8, 4, 9},
+                    2,
                     8
                 },
                 new object[]
                 {
-                    new int[] { 1,3,7,5,10,3 },
-                    3, 
+                    new[] {1, 3, 7, 5, 10, 3},
+                    3,
                     6
-                },
+                }
             };
         }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Best_time_to_buy_and_sell_stock_with_transaction_fee_tests(int[] prices, int fee, int expected)
+    {
+        var result = new BestTimeToBuyAndSellStockWithTransactionFee().MaxProfitWithFee(prices, fee);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Best_time_to_buy_and_sell_stock_with_transaction_fee_top_down_tests(int[] prices, int fee, int expected)
+    {
+        var result = new BestTimeToBuyAndSellStockWithTransactionFee().MatProfitWithFeeTopDown(prices, fee);
+        result.Should().Be(expected);
     }
 }
--- Problems/Series/BestTimeToBuyStock/BestTimeToBuyAndSellStockWithTransactionFee.cs
+++ Problems/Series/BestTimeToBuyStock/BestTimeToBuyAndSellStockWithTransactionFee.cs
@@ -2,8 +2,59 @@
 
 public class BestTimeToBuyAndSellStockWithTransactionFee
 {
+    private readonly Dictionary<(int, bool), int> _cache = new();
+
     public int MaxProfitWithFee(int[] prices, int fee)
     {
-        
+        var n = prices.Length;
+        if (n <= 1)
+            return 0;
+        var dp = new int[n][];
+        for (var i = 0; i < n; i++)
+            dp[i] = new int[2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0] - fee; // means we bought it today
+
+        for (var i = 1; i < n; i++)
+        {
+            dp[i][0] = Math.Max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.Max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+
+        return dp[^1][0];
+    }
+
+    public int MatProfitWithFeeTopDown(int[] prices, int fee)
+    {
+        return Dfs(prices, 0, fee, true);
+    }
+
+    private int Dfs(int[] prices, int index, int fee, bool buying)
+    {
+        if (index >= prices.Length)
+            return 0;
+
+        if (_cache.ContainsKey((index, buying)))
+            return _cache[(index, buying)];
+
+        var max = 0;
+        if (buying)
+        {
+            var buy = -prices[index] - fee + Dfs(prices, index + 1, fee, false);
+            var notBuy = Dfs(prices, index + 1, fee, true);
+            max = Math.Max(buy, notBuy);
+        }
+        else
+        {
+            var sell = prices[index] + Dfs(prices, index + 1, fee, true);
+            var notSell = Dfs(prices,index + 1,fee ,false);
+            max = Math.Max(sell, notSell);
+            
+        }
+
+        _cache[(index, buying)] = max;
+
+        return max;
     }
 }
