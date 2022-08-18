65bcc1b70c6b4d031cef84dd8d5a2ea9c2975432
Panagiotis <panoskarajohn@hotmail.com>
2022-08-18 22:59:09 +0300

    Count vowels

---
+++ Problem.Tests/DP/CountVowelsTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class CountVowelsTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    1, 5
+                },
+                new object[]
+                {
+                    2, 10
+                },
+                new object[]
+                {
+                    5, 68
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Count_vowels_tests(int n, int expected)
+    {
+        var result = new CountVowels().CountVowelPermutation(n);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/CountVowels.cs
@@ -0,0 +1,34 @@
+﻿namespace Problems.DP;
+
+public class CountVowels
+{
+    private const int MODULO = 1_000_000_007;
+    public int CountVowelPermutation(int n)
+    {
+        var aVowel = new long[n];
+        var eVowel = new long[n];
+        var iVowel = new long[n];
+        var oVowel = new long[n];
+        var uVowel = new long[n];
+
+        aVowel[0] = 1;
+        eVowel[0] = 1;
+        iVowel[0] = 1;
+        oVowel[0] = 1;
+        uVowel[0] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            aVowel[i] = (eVowel[i - 1] + iVowel[i - 1] + uVowel[i - 1]) % MODULO;
+            eVowel[i] = (aVowel[i - 1] + iVowel[i - 1]) % MODULO;
+            iVowel[i] = (eVowel[i - 1] + oVowel[i - 1]) % MODULO;
+            oVowel[i] = iVowel[i - 1] % MODULO;
+            uVowel[i] = (iVowel[i - 1] + oVowel[i - 1]) % MODULO;
+        }
+        
+        long result = 
+            (aVowel[n - 1] + eVowel[n - 1] + iVowel[n - 1] + oVowel[n - 1] + uVowel[n - 1]) % MODULO;
+        return (int)result;
+    }
+    
+    
+}
