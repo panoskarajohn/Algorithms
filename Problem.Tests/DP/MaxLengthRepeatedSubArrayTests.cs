c833e40daf2d74fba8ceee17bd56c07684776c45
Panagiotis <panoskarajohn@hotmail.com>
2022-08-19 16:29:27 +0300

    MaxLengthRepeatedSubArray

---
+++ Problem.Tests/DP/MaxLengthRepeatedSubArrayTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class MaxLengthRepeatedSubArrayTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 2, 3, 2, 1},
+                    new[] {3, 2, 1, 4, 7},
+                    3
+                },
+                new object[]
+                {
+                    new[] {0, 0, 0, 0, 0},
+                    new[] {0, 0, 0, 0, 0},
+                    5
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Max_length_repeated_sub_array_tests(int[] nums1, int[] nums2, int expected)
+    {
+        var result = new MaxLengthRepeatedSubArray()
+            .FindLengthBottomUp(nums1, nums2);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/MaxLengthRepeatedSubArray.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.DP;
+
+public class MaxLengthRepeatedSubArray
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int FindLengthBottomUp(int[] nums1, int[] nums2)
+    {
+        var answer = 0;
+        var n = nums1.Length;
+        var m = nums2.Length;
+
+        var memo = new int[n + 1][];
+        for (var i = 0; i < n + 1; i++) memo[i] = new int[m + 1];
+
+        for (var i = n - 1; i >= 0; i--)
+        for (var j = m - 1; j >= 0; j--)
+            if (nums1[i] == nums2[j])
+            {
+                memo[i][j] = memo[i + 1][j + 1] + 1;
+                answer = Math.Max(answer, memo[i][j]);
+            }
+
+        return answer;
+    }
+}
