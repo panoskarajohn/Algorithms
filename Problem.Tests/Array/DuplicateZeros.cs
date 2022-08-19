98e7ac97297a7df547636d6a7833bafc5e4b98f9
Panagiotis <panoskarajohn@hotmail.com>
2022-08-19 21:35:55 +0300

    Arrays duplicate zeros & valid mountain array

---
+++ Problem.Tests/Array/DuplicateZeros.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+
+namespace Problem.Tests.Array;
+
+public class DuplicateZeros
+{
+    public void DupZeros(int[] arr)
+    {
+        var prev = 0;
+        for (var i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] != 0)
+                continue;
+
+            for (var j = i + 1; j < arr.Length; j++) (arr[j], prev) = (prev, arr[j]);
+
+            i++;
+            prev = 0;
+        }
+    }
+}
+
+public class DuplicateZerosTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 0, 2, 3, 0, 4, 5, 0},
+                    new[] {1, 0, 0, 2, 3, 0, 0, 4}
+                },
+                new object[]
+                {
+                    new[] {8, 5, 0, 9, 0, 3, 4, 7},
+                    new[] {8, 5, 0, 0, 9, 0, 0, 3}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Duplicate_zeros_tests(int[] data, int[] expected)
+    {
+        var s = new DuplicateZeros();
+        s.DupZeros(data);
+        data.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problem.Tests/Array/ValidMountainArray.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+
+namespace Problem.Tests.Array;
+
+public class ValidMountainArrayTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {0, 3, 2, 1},
+                    true
+                },
+                new object[]
+                {
+                    new[] {1, 3, 2},
+                    true
+                },
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Valid_mountain_array(int[] data, bool expected)
+    {
+        var result = new ValidMountainArray().IsValid(data);
+        result.Should().Be(expected);
+    }
+
+    public class ValidMountainArray
+    {
+        public bool IsValid(int[] arr)
+        {
+            var n = arr.Length;
+            if (n < 3) return false;
+            var increased = false;
+            var index = -1;
+
+            for (var i = 0; i < n - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                    return false;
+
+                if (arr[i] < arr[i + 1])
+                    increased = true;
+
+                if (arr[i] > arr[i + 1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (!increased || index == -1)
+                return false;
+            var decreased = false;
+            for (var i = index; i < n - 1; i++)
+            {
+                if (arr[i] <= arr[i + 1])
+                    return false;
+
+                if (arr[i] > arr[i + 1]) decreased = true;
+            }
+
+            return true;
+        }
+    }
+}
