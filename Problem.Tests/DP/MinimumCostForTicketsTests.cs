a297af10f5d1f60f99f511c1ca359b10f3e84297
Panagiotis <panoskarajohn@hotmail.com>
2022-08-20 23:19:04 +0300

    Minimum Cost for tickets

---
+++ Problem.Tests/DP/MinimumCostForTicketsTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Problems.DP;
+
+namespace Problem.Tests.DP;
+
+public class MinimumCostForTicketsTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 4, 6, 7, 8, 20},
+                    new[] {2, 7, 15},
+                    11
+                },
+                new object[]
+                {
+                    new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31},
+                    new[] {2, 7, 15},
+                    17
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Minimum_cost_for_tickets_tests(int[] days, int[] costs, int expected)
+    {
+        var result = new MinimumCostForTickets().MinCostTicketsTopDown(days, costs);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Minimum_cost_for_tickets_Bottom_Up_tests(int[] days, int[] costs, int expected)
+    {
+        var result = new MinimumCostForTickets().MinCostTicketsBottomUp(days, costs);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/DP/MinimumCostForTickets.cs
@@ -0,0 +1,57 @@
+﻿namespace Problems.DP;
+
+public class MinimumCostForTickets
+{
+    private readonly Dictionary<int, int> _cache = new();
+    private readonly Dictionary<int, int> _daysCosts = new();
+
+    public int MinCostTicketsTopDown(int[] days, int[] costs)
+    {
+        _daysCosts[1] = costs[0];
+        _daysCosts[7] = costs[1];
+        _daysCosts[30] = costs[2];
+        return Dfs(days, 0);
+    }
+
+    private int Dfs(int[] days, int index)
+    {
+        if (index == days.Length) return 0;
+        if (_cache.ContainsKey(index)) return _cache[index];
+
+        _cache[index] = int.MaxValue;
+
+        foreach (var daysFromCost in _daysCosts.Keys)
+        {
+            var j = index;
+            while (j < days.Length && days[j] < days[index] + daysFromCost) j += 1;
+
+            _cache[index] = Math.Min(_cache[index], _daysCosts[daysFromCost] + Dfs(days, j));
+        }
+
+        return _cache[index];
+    }
+
+
+    public int MinCostTicketsBottomUp(int[] days, int[] costs)
+    {
+        var n = days.Length;
+        var dp = new Dictionary<int, int>();
+        _daysCosts[1] = costs[0];
+        _daysCosts[7] = costs[1];
+        _daysCosts[30] = costs[2];
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            dp[i] = int.MaxValue;
+            foreach (var daysCosts in _daysCosts.Keys)
+            {
+                var j = i;
+                while (j < n && days[j] < days[i] + daysCosts) j++;
+
+                dp[i] = Math.Min(dp[i], _daysCosts[daysCosts] + dp.GetValueOrDefault(j));
+            }
+        }
+
+        return dp[0];
+    }
+}
