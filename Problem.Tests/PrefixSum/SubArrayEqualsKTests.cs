28d6ea946cbef57faaa3e893e5103a251ca96b76
Panagiotis <panoskarajohn@hotmail.com>
2022-08-20 22:23:55 +0300

    SubArrayEqualK Prefix sum

---
+++ Problem.Tests/PrefixSum/SubArrayEqualsKTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Problems.PrefixSum;
+
+namespace Problem.Tests.PrefixSum;
+
+public class SubArrayEqualsKTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {1, 1, 1},
+                    2,
+                    2
+                },
+                new object[]
+                {
+                    new[] {1, 2, 3},
+                    3,
+                    2
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Sub_array_equals_k_tests(int[] data, int k, int expected)
+    {
+        var result = new SubArrayEqualsK().SubarraySum(data, k);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/PrefixSum/SubArrayEqualsK.cs
@@ -0,0 +1,32 @@
+﻿namespace Problems.PrefixSum;
+
+public class SubArrayEqualsK
+{
+    /// <summary>
+    ///     Prefix Sum
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int SubarraySum(int[] nums, int k)
+    {
+        var n = nums.Length;
+        var map = new Dictionary<int, int>();
+        var count = 0;
+        var sum = 0;
+
+        map[0] = 1; //case of 1 + 1 + 1 - 3 = 0
+        for (var i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            var search = sum - k;
+            if (map.ContainsKey(search)) count += map[search];
+
+            if (!map.ContainsKey(sum))
+                map[sum] = 0;
+            map[sum]++;
+        }
+
+        return count;
+    }
+}
