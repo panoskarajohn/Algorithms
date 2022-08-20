d956ba60e1c85f313bc9f8ba1021dfe42275e527
Panagiotis <panoskarajohn@hotmail.com>
2022-08-20 12:57:58 +0300

    Array

---
+++ Problem.Tests/Array/ReplaceElementsWIthGreatestElementOnRightSide.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using FluentAssertions;
+
+namespace Problem.Tests.Array;
+
+public class ReplaceElementsWIthGreatestElementOnRightSideTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {17, 18, 5, 4, 6, 1},
+                    new[] {18, 6, 6, 6, 1, -1}
+                },
+                new object[]
+                {
+                    new[] {400},
+                    new[] {-1}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Replace_elements_w_ith_greatest_element_on_right_side_tests(int[] data, int[] expected)
+    {
+        var result = new ReplaceElementsWIthGreatestElementOnRightSide().ReplaceElements(data);
+        result.Should().BeEquivalentTo(expected);
+        
+    }
+}
+
+public class ReplaceElementsWIthGreatestElementOnRightSide
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        int n = arr.Length;
+        int maxRight = arr[^1];
+        arr[^1] = -1;
+        
+        for (int i = n - 2; i >= 0; i--)
+        {
+            var current = arr[i];
+            arr[i] = maxRight;
+            maxRight = Math.Max(maxRight, current);
+        }
+
+        return arr;
+    }
+}
---
+++ Problem.Tests/Array/ThirdMaxNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+using FluentAssertions;
+
+namespace Problem.Tests.Array;
+
+public class ThirdMaxNumberTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Third_max_number_tests(int[] data, int expected)
+    {
+        var result = new ThirdMaxNumber().ThirdMax(data);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 3,2,1 }, 
+                    1
+                },
+                new object[]
+                {
+                    new int[] { 1,2 }, 
+                    2
+                },
+                new object[]
+                {
+                    new int[] { 2,2,3,1 }, 
+                    1
+                },
+                new object[]
+                {
+                    new int[] { 1,2,int.MinValue }, 
+                    int.MinValue
+                },
+                
+                new object[]
+                {
+                    new int[] { 1,int.MinValue, 2 }, 
+                    int.MinValue
+                },
+            };
+        }
+    }
+}
+
+public class ThirdMaxNumber
+{
+    public int ThirdMax(int[] nums)
+    {
+        var set = new HashSet<int>(nums); // O(n)
+        var max = set.Max();
+        if (set.Count < 3)
+            return max;
+
+        set.Remove(max);
+        max = set.Max();
+        set.Remove(max);
+        return set.Max();
+
+        
+    }
+}
