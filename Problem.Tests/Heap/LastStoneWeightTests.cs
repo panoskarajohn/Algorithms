3164bc01d755cd9709576a395fd74ecb11a911b0
Panagiotis <panoskarajohn@hotmail.com>
2022-08-22 18:58:27 +0300

    Find last stone weight

---
+++ Problem.Tests/Heap/LastStoneWeightTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+public class LastStoneWeightTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Last_Stone_Weight_Tests(int[] stones, int expected)
+    {
+        var result = new FindLastStoneWeight().Get(stones);
+        result.Should().Be(expected);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 2,7,4,1,8,1 },
+                    1
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Heap/FindLastStoneWeight.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Heap;
+
+public class FindLastStoneWeight
+{
+    public int Get(int[] stones)
+    {
+        var heap = new PriorityQueue<int, int>();
+
+        foreach (var t in stones) heap.Enqueue(t, -t);
+
+        while (heap.Count > 1)
+        {
+            var highest = heap.Dequeue();
+            var secondHighest = heap.Dequeue();
+
+            if (highest == secondHighest)
+                continue;
+            var queueItem = highest - secondHighest;
+            heap.Enqueue(queueItem, -queueItem);
+        }
+
+        return heap.Count > 0 ? heap.Peek() : 0;
+    }
+}
