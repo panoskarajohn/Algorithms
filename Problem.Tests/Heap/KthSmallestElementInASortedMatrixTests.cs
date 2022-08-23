5f830b99ab9a7dd32157901cc7732d4a3f6329ce
Panagiotis <panoskarajohn@hotmail.com>
2022-08-23 20:27:08 +0300

    kth smallest element in a sorted array

---
+++ Problem.Tests/Heap/KthSmallestElementInASortedMatrixTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+
+public class KthSmallestElementInASortedMatrixTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Kth_smallest_element_in_a_sorted_matrix_tests(int[][] data, int k, int exp)
+    {
+        var result = new KthSmallestElementInASortedMatrix().KthSmallest(data, k);
+        result.Should().Be(exp);
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {new[] {1, 5, 9}, new[] {10, 11, 13}, new[] {12, 13, 15}},
+                    8,
+                    13
+                }
+            };
+        }
+    }
+}
---
+++ Problems/Heap/KthSmalletElementInASortedMatrix.cs
@@ -0,0 +1,22 @@
+﻿namespace Problems.Heap;
+
+public class KthSmallestElementInASortedMatrix
+{
+    public int KthSmallest(int[][] matrix, int k)
+    {
+        var pq = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                pq.Enqueue(matrix[i][j], -matrix[i][j]);
+                if (pq.Count > k)
+                    pq.Dequeue();
+            }
+        }
+
+        return pq.Peek();
+
+    }
+}
