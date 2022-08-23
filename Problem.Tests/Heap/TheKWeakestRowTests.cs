f7f11d81e76492c51ddb6c5cf3f0c98174fd4cfd
Panagiotis <panoskarajohn@hotmail.com>
2022-08-23 19:05:40 +0300

    kthweakestrow

---
+++ Problem.Tests/Heap/TheKWeakestRowTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+public class TheKWeakestRowTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] {1, 1, 0, 0, 0},
+                        new[] {1, 1, 1, 1, 0},
+                        new[] {1, 0, 0, 0, 0},
+                        new[] {1, 1, 0, 0, 0},
+                        new[] {1, 1, 1, 1, 1}
+                    },
+                    3,
+                    new[] {2, 0, 3}
+                },
+                new object[]
+                {
+                    new[]
+                    {
+                        new[] {1, 0, 0, 0},
+                        new[] {1, 1, 1, 1},
+                        new[] {1, 0, 0, 0},
+                        new[] {1, 0, 0, 0}
+                    },
+                    2,
+                    new[] {0, 2}
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void The_k_weakest_row_tests(int[][] data, int k, int[] expected)
+    {
+        var result = new FindKWeakestRow().KWeakestRows(data, k);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Heap/FindKWeakestRow.cs
@@ -0,0 +1,49 @@
+﻿namespace Problems.Heap;
+
+public class FindKWeakestRow
+{
+    public int[] KWeakestRows(int[][] mat, int k)
+    {
+        var rows = mat.Length;
+        var matPairs = new int[rows][];
+        for (int i = 0; i < rows; i++)
+        {
+            matPairs[i] = new int[2];
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            var soldiers = 0;
+            for (int j = 0; j < mat[i].Length; j++)
+            {
+                if(mat[i][j] == 0) break;
+                soldiers++;
+            }
+
+            matPairs[i][0] = soldiers;
+            matPairs[i][1] = i;
+        }
+        
+        System.Array.Sort(matPairs, (a, b) =>
+        {
+            if (a[0] == b[0])
+            {
+                return a[1].CompareTo(b[1]);
+            }
+
+            return a[0].CompareTo(b[0]);
+        });
+
+        int[] indexes = new int[k];
+
+        for (int i = 0; i < k; i++)
+        {
+            indexes[i] = matPairs[i][1];
+        }
+
+        return indexes;
+
+    }
+
+    
+}
