b391d2e0f2a5b77ffb78b0125ec0bdd887741aed
Panagiotis <panoskarajohn@hotmail.com>
2022-08-23 20:27:26 +0300

    Min cost to connect sticks

---
+++ Problem.Tests/Heap/MinCostToConnectSticksTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+public class MinCostToConnectSticksTests
+{
+    [Theory, MemberData(nameof(TestDataProperty))]
+    public void Min_cost_to_connect_sticks_tests(int[] sticks, int exp)
+    {
+        var result = new MinCostToConnectSticks().ConnectSticks(sticks);
+        result.Should().Be(exp);
+
+    }
+    
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[] { 1,8,3,5 },
+                    30
+                },
+                new object[]
+                {
+                    new int[] { 2,4,3 },
+                    14
+                },
+            };
+        }
+    }
+}
---
+++ Problems/Heap/MinCostToConnectSticks.cs
@@ -0,0 +1,26 @@
+﻿namespace Problems.Heap;
+
+public class MinCostToConnectSticks
+{
+    public int ConnectSticks(int[] sticks) {
+        if(sticks.Length < 1)
+            return 0;
+        
+        var pq = new PriorityQueue<int, int>();
+        
+        foreach(var stick in sticks)
+        {
+            pq.Enqueue(stick, stick);
+        }
+        int cost = 0;
+        while(pq.Count > 1)
+        {
+            var first  = pq.Dequeue();
+            var second = pq.Dequeue();
+            var add = first + second;
+            cost += add;
+            pq.Enqueue(add, add);
+        }
+        return cost;
+    }
+}
