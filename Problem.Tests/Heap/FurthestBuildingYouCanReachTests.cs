195e24bccc7d176573604230e25f741db828e34c
Panagiotis <panoskarajohn@hotmail.com>
2022-08-23 23:17:06 +0300

    FurthestBuildingYouCanReach.cs

---
+++ Problem.Tests/Heap/FurthestBuildingYouCanReachTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+public class FurthestBuildingYouCanReachTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {4, 12, 2, 7, 3, 18, 20, 3, 19},
+                    10,
+                    2,
+                    7
+                },
+                new object[]
+                {
+                    new[] {14, 3, 19, 3},
+                    17,
+                    0,
+                    3
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Furthest_building_you_can_reach_tests(int[] data, int bricks, int ladders, int expected)
+    {
+        var result = new FurthestBuildingYouCanReach().FurthestBuilding(data, bricks, ladders);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/Heap/FurthestBuildingYouCanReach.cs
@@ -0,0 +1,50 @@
+﻿namespace Problems.Heap;
+
+public class FurthestBuildingYouCanReach
+{
+    private readonly Dictionary<(int, int, int), int> _cache = new();
+
+    /// <summary>
+    ///     Of course Dfs solutoin out of memory
+    /// </summary>
+    /// <param name="heights"></param>
+    /// <param name="bricks"></param>
+    /// <param name="ladders"></param>
+    /// <returns></returns>
+    public int FurthestBuilding(int[] heights, int bricks, int ladders)
+    {
+        return Dfs(0, bricks, ladders, heights);
+    }
+
+    private int Dfs(int index, int bricks, int ladders, int[] heights)
+    {
+        if ((bricks < 0 && ladders < 0) || index == heights.Length - 1) return index;
+
+        if (_cache.ContainsKey((index, bricks, ladders)))
+            return _cache[(index, bricks, ladders)];
+
+        var currentHeight = heights[index];
+        var nextHeight = heights[index + 1];
+
+        var countSteps = 0;
+        var countStepsWithBricks = 0;
+        var countStepsWithLadders = 0;
+        if (currentHeight >= nextHeight)
+        {
+            countSteps = Dfs(index + 1, bricks, ladders, heights);
+        }
+        else
+        {
+            countStepsWithBricks = bricks - (nextHeight - currentHeight) >= 0
+                ? Dfs(index + 1, bricks - (nextHeight - currentHeight), ladders, heights)
+                : 0;
+
+            countStepsWithLadders = ladders - 1 >= 0
+                ? Dfs(index + 1, bricks, ladders - 1, heights)
+                : 0;
+        }
+
+        var max = Math.Max(Math.Max(countSteps, index), Math.Max(countStepsWithBricks, countStepsWithLadders));
+        return _cache[(index, bricks, ladders)] = max;
+    }
+}
