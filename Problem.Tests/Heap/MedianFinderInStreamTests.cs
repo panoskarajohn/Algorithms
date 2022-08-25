467a80e1aa0d3ebb09c7e58f65880549e85181a7
Panagiotis <panoskarajohn@hotmail.com>
2022-08-25 21:30:55 +0300

    Median Finder in data stream

---
+++ Problem.Tests/Heap/MedianFinderInStreamTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Problems.Heap;
+
+namespace Problem.Tests.Heap;
+
+public class MedianFinderInStreamTests
+{
+    [Fact]
+    public void Median_Finder_In_Stream_Tests()
+    {
+        var median = new MedianFinder();
+        median.AddNum(1);
+        median.AddNum(2);
+        median.FindMedian().Should().Be(1.50000);
+        median.AddNum(3);
+        median.FindMedian().Should().Be(2.0000);
+    }
+
+    [Fact]
+    public void Median_Finder_In_Stream_Negative_Tests()
+    {
+        var median = new MedianFinder();
+        median.AddNum(-1);
+        median.FindMedian().Should().Be(-1.00000);
+        median.AddNum(-2);
+        median.FindMedian().Should().Be(-1.50000);
+        median.AddNum(-3);
+        median.FindMedian().Should().Be(-2.0000);
+        median.AddNum(-4);
+        median.FindMedian().Should().Be(-2.5000);
+        median.AddNum(-5);
+        median.FindMedian().Should().Be(-3.0000);
+    }
+
+    [Fact]
+    public void Median_Finder_In_Stream_Two_Heaps_Tests()
+    {
+        var median = new MedianFinderTwoHeaps();
+        median.AddNum(1);
+        median.AddNum(2);
+        median.FindMedian().Should().Be(1.50000);
+        median.AddNum(3);
+        median.FindMedian().Should().Be(2.0000);
+    }
+
+    [Fact]
+    public void Median_Finder_In_Stream_Two_Heaps_Negative_Tests()
+    {
+        var median = new MedianFinderTwoHeaps();
+        median.AddNum(-1);
+        median.FindMedian().Should().Be(-1.00000);
+        median.AddNum(-2);
+        median.FindMedian().Should().Be(-1.50000);
+        median.AddNum(-3);
+        median.FindMedian().Should().Be(-2.0000);
+        median.AddNum(-4);
+        median.FindMedian().Should().Be(-2.5000);
+        median.AddNum(-5);
+        median.FindMedian().Should().Be(-3.0000);
+    }
+}
---
+++ Problems/Heap/MedianFinderInStream.cs
@@ -0,0 +1,57 @@
+﻿namespace Problems.Heap;
+
+/// <summary>
+///     Easy solution and with the worst complexity
+/// </summary>
+public class MedianFinder
+{
+    private readonly List<int> _store;
+
+    public MedianFinder()
+    {
+        _store = new List<int>();
+    }
+
+    public void AddNum(int num)
+    {
+        _store.Add(num);
+    }
+
+    public double FindMedian()
+    {
+        var n = _store.Count;
+        _store.Sort();
+        return n % 2 == 1 ? _store[n / 2] : (_store[n / 2 - 1] + _store[n / 2]) * 0.5;
+    }
+}
+
+public class MedianFinderTwoHeaps
+{
+    private readonly PriorityQueue<int, int> _maxHeap;
+    private readonly PriorityQueue<int, int> _minHeap;
+
+    public MedianFinderTwoHeaps()
+    {
+        _minHeap = new PriorityQueue<int, int>();
+        _maxHeap = new PriorityQueue<int, int>();
+    }
+
+    public void AddNum(int num)
+    {
+        _maxHeap.Enqueue(num, -num);
+        var max = _maxHeap.Dequeue();
+        _minHeap.Enqueue(max, max);
+        if (_minHeap.Count > _maxHeap.Count)
+        {
+            var min = _minHeap.Dequeue();
+            _maxHeap.Enqueue(min, -min);
+        }
+    }
+
+    public double FindMedian()
+    {
+        if (_minHeap.Count == _maxHeap.Count)
+            return (_maxHeap.Peek() + _minHeap.Peek()) * 0.5;
+        return _maxHeap.Peek();
+    }
+}
