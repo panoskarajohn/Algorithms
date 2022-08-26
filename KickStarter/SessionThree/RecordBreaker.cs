8098fe3b8ba362c370f728f5ebe77dc7e97777d6
Panagiotis <panoskarajohn@hotmail.com>
2022-08-26 22:38:31 +0300

    Kickstart record breaker and wiggle walk

---
+++ KickStarter/SessionThree/RecordBreaker.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+
+namespace KickStarter.SessionThree;
+
+public class RecordBreaker
+{
+    /// <summary>
+    ///     Isyana is given the number of visitors at her local theme park on N consecutive days.
+    ///     The number of visitors on the i-th day is Vi. A day is record breaking
+    ///     if it satisfies both of the following conditions:
+    ///     The number of visitors on the day is strictly larger than the number
+    ///     of visitors on each of the previous days.
+    ///     Either it is the last day, or the number of visitors on the day is strictly larger than the number of visitors on
+    ///     the following day.
+    ///     Note that the very first day could be a record breaking day!
+    ///     Examples:
+    ///     8
+    ///     _   _
+    ///     1 2 0 7 2 0 2 0 => 2
+    ///     6
+    ///     _
+    ///     4 8 15 16 23 42 => 1
+    ///     9
+    ///     _   _     _
+    ///     3 1 4 1 5 9 2 6 5 => 3
+    ///     6
+    ///     9 9 9 9 9 9 => 0
+    /// </summary>
+    public int GetWorldBreakingDay(int numOfDays, int[] visitors)
+    {
+        var anwer = 0;
+        var max = int.MinValue;
+        var count = new int[numOfDays];
+        for (var i = 0; i < numOfDays; i++)
+        {
+            if (visitors[i] > max)
+            {
+                max = visitors[i];
+                count[i]++;
+            }
+
+            if (i == numOfDays - 1)
+                count[i]++;
+            else if (visitors[i] > visitors[i + 1]) count[i]++;
+
+            if (count[i] == 2) anwer++;
+        }
+
+        return anwer;
+    }
+}
+
+public class RecordBreakerTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    8,
+                    new[] {1, 2, 0, 7, 2, 0, 2, 0},
+                    2
+                },
+                new object[]
+                {
+                    6,
+                    new[] {4, 8, 15, 16, 23, 42},
+                    1
+                },
+                new object[]
+                {
+                    9,
+                    new[] {3, 1, 4, 1, 5, 9, 2, 6, 5},
+                    3
+                },
+                new object[]
+                {
+                    6,
+                    new[] {9, 9, 9, 9, 9, 9},
+                    0
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Record_breaker_tests(int numberOfDays, int[] visitors, int expected)
+    {
+        var result = new RecordBreaker().GetWorldBreakingDay(numberOfDays, visitors);
+        result.Should().Be(expected);
+    }
+}
---
+++ KickStarter/SessionThree/WiggleWalk.cs
@@ -0,0 +1,115 @@
+﻿using FluentAssertions;
+
+namespace KickStarter.SessionThree;
+
+public class WiggleWalk
+{
+    private int _cols;
+
+    Dictionary<char, int[]> _directionMap = new()
+    {
+        {'N', new[] {-1, 0}},
+        {'E', new[] {0, 1}},
+        {'W', new[] {0, -1}},
+        {'S', new[] {1, 0}}
+    };
+
+    private int _rows;
+
+    public int[] GetLastPositionAfterInstructions(
+        int numberOfInstructions,
+        int rows,
+        int columns,
+        int startingRow,
+        int startingColumn,
+        string instructions)
+    {
+        _rows = rows;
+        _cols = columns;
+
+        var (row, col) = (startingRow, startingColumn);
+        var visited = new HashSet<(int, int)>();
+        var cache = new Dictionary<(char, int, int), (int, int)>();
+
+        for (var i = 0; i < numberOfInstructions; i++) (row, col) = Move(instructions[i], row, col, visited, cache);
+
+        return new[] {row, col};
+    }
+
+    private (int row, int col) Move(char instruction, int currentRow, int currentCol,
+        HashSet<(int row, int col)> visited, Dictionary<(char, int, int), (int, int)> cache)
+    {
+        visited.Add((currentRow, currentCol));
+        (int row, int col) destination;
+        if (cache.ContainsKey((instruction, currentRow, currentCol)))
+            destination = cache[(instruction, currentRow, currentCol)];
+        else
+        {
+            destination =  GetDestination(instruction, currentRow, currentCol); 
+        }
+        
+        while (visited.Contains(destination))
+        {
+            var temp = GetDestination(instruction, destination.row, destination.col);
+            if (temp.row < 0 || temp.row > _rows || temp.col < 0 || temp.col > _cols)
+            {
+                visited.Add(destination);
+                cache[(instruction, currentRow, currentCol)] = destination;
+                return destination;
+            }
+
+            destination = temp;
+        }
+
+        visited.Add(destination);
+        return cache[(instruction, currentRow, currentCol)] = destination;
+    }
+
+    private (int row, int col) GetDestination(char instruction, int currentRow, int currentCol)
+    {
+        var direction = _directionMap[instruction];
+        return (currentRow + direction[0], currentCol + direction[1]);
+    }
+}
+
+public class WiggleWalkTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    5, 3, 6, 2, 3,
+                    "EEWNS",
+                    3, 2
+                },
+                new object[]
+                {
+                    4, 3, 3, 1, 1,
+                    "SESE",
+                    3, 3
+                },
+                new object[]
+                {
+                    11, 5, 8, 3, 4,
+                    "NEESSWWNESE",
+                    3, 7
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Wiggle_walk_tests(int numberOfInstructions, int rows, int columns, int startingRow, int startingColumn,
+        string instrustions, int expectedRow, int expectedColumn)
+    {
+        var result = new WiggleWalk().GetLastPositionAfterInstructions(numberOfInstructions, rows, columns, startingRow,
+            startingColumn, instrustions);
+        result[0].Should().Be(expectedRow);
+        result[1].Should().Be(expectedColumn);
+    }
+}
