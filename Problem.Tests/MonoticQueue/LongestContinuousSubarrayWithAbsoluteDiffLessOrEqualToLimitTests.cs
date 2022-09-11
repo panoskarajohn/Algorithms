047931e5aee4682b6cb1a59fbcf05b42dbaa9f0c
Panagiotis <panoskarajohn@hotmail.com>
2022-09-11 15:35:31 +0300

    Longest continuous Subarray with absolute diff less or equal to limit

---
+++ Problem.Tests/MonoticQueue/LongestContinuousSubarrayWithAbsoluteDiffLessOrEqualToLimitTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Problems.MonotonicQueue;
+
+namespace Problem.Tests.MonoticQueue;
+
+public class LongestContinuousSubarrayWithAbsoluteDiffLessOrEqualToLimitTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {8, 2, 4, 7},
+                    4,
+                    2
+                },
+                new object[]
+                {
+                    new[] {10, 1, 2, 4, 7, 2},
+                    5,
+                    4
+                },
+                new object[]
+                {
+                    new[] {4, 2, 2, 2, 4, 4, 2, 2},
+                    0,
+                    3
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Longest_Continuous_Subarray_With_Absolute_Diff_Less_Or_Equal_To_Limit_Tests(int[] nums, int limit,
+        int expected)
+    {
+        var result = new LongestContinuousSubarrayWithAbsoluteDiffLessOrEqualToLimit()
+            .LongestSubarray(nums, limit);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/MonotonicQueue/LongestContinuousSubarrayWithAbsoluteDiffLessOrEqualToLimit.cs
@@ -0,0 +1,46 @@
+﻿using Problems.DataStructures;
+
+namespace Problems.MonotonicQueue;
+
+public class LongestContinuousSubarrayWithAbsoluteDiffLessOrEqualToLimit
+{
+    public int LongestSubarray(int[] nums, int limit)
+    {
+        // C# does not contain a class for Deque(double ended queue)
+        // We need to mock it
+        var minQueue = new LinkedList<int>();
+        var maxQueue = new LinkedList<int>();
+
+        int n = nums.Length;
+        int left = 0;
+        int right = 0;
+        int best = int.MinValue;
+
+        while (right < n)
+        {
+            while(maxQueue.Any() && maxQueue!.Last!.Value < nums[right])
+                maxQueue.RemoveLast();
+
+            maxQueue.AddLast(nums[right]);
+            
+            while(minQueue.Any() && minQueue!.Last!.Value > nums[right])
+                minQueue.RemoveLast();
+
+            minQueue.AddLast(nums[right]);
+
+            while (maxQueue!.First!.Value - minQueue!.First!.Value > limit)
+            {
+                if(maxQueue.First.Value == nums[left]) maxQueue.RemoveFirst();
+                if(minQueue.First.Value == nums[left]) minQueue.RemoveFirst();
+
+                left++;
+            }
+
+            best = Math.Max(best, right - left + 1);
+            right++;
+        }
+
+        return best;
+
+    }
+}
