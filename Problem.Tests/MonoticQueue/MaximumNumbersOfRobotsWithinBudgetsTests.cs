dfe1d84437030db8c57425a91b12eb9ab21d9db6
Panagiotis <panoskarajohn@hotmail.com>
2022-09-11 16:23:38 +0300

    My solution which is wrong. It passes the example scenarios but was failing all the other scenarios. Committed for historic reasons

---
+++ Problem.Tests/MonoticQueue/MaximumNumbersOfRobotsWithinBudgetsTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Problems.MonotonicQueue;
+
+namespace Problem.Tests.MonoticQueue;
+
+public class MaximumNumbersOfRobotsWithinBudgetsTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new[] {3, 6, 1, 3, 4},
+                    new[] {2, 1, 3, 4, 5},
+                    25,
+                    3
+                },
+                new object[]
+                {
+                    new[] {11, 12, 19},
+                    new[] {10, 8, 7},
+                    19,
+                    0
+                },
+                new object[]
+                {
+                    new[] {11, 12, 74, 67, 37, 87, 42, 34, 18, 90, 36, 28, 34, 20},
+                    new[] {18, 98, 2, 84, 7, 57, 54, 65, 59, 91, 7, 23, 94, 20},
+                    937,
+                    4
+                },
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Maximum_Numbers_Of_Robots_Within_Bufdgets_Tests(int[] chargeTime,
+        int[] runningCosts, int budget, int expected)
+    {
+        var result = new MaximumNumbersOfRobotsWithinBudgets()
+            .MaximumRobots(chargeTime, runningCosts, budget);
+        result.Should().Be(expected);
+    }
+}
---
+++ Problems/MonotonicQueue/MaximumNumbersOfRobotsWithinBudgets.cs
@@ -0,0 +1,32 @@
+﻿namespace Problems.MonotonicQueue;
+
+public class MaximumNumbersOfRobotsWithinBudgets
+{
+    public int MaximumRobots(int[] chargeTimes, int[] runningCosts, long budget)
+    {
+        var max = int.MinValue;
+        var n = chargeTimes.Length;
+        var left = 0;
+        var right = n - 1;
+
+        while (left < right)
+        {
+            var subCharge = chargeTimes[left..right];
+            var subRunningCosts = runningCosts[left..right];
+            long k = chargeTimes.Length;
+
+            var totalCost = subCharge.Max() + k * subRunningCosts.Sum();
+
+            if (totalCost <= budget)
+                if (right - left + 1 > max)
+                    max = right - left + 1;
+
+            if (runningCosts[left] + chargeTimes[left] < runningCosts[right] + chargeTimes[right])
+                right--;
+            else
+                left++;
+        }
+
+        return max == int.MinValue ? 0 : max;
+    }
+}
