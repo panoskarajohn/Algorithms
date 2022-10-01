bf3cb7b74dec08a309cdac2515578c632d8322c9
Panagiotis <panoskarajohn@hotmail.com>
2022-10-01 12:30:42 +0300

    Bank Transfer tests

---
+++ Problem.Tests/Microsoft/BankTransferTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Problems.Microsoft;
+
+namespace Problem.Tests.Microsoft;
+
+public class BankTransferTests
+{
+    public static IEnumerable<object[]> TestDataProperty
+    {
+        get
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    "BAABA",
+                    new int[] { 2,4,1,1,2 },
+                    new int[] { 2, 4 }
+                },
+                new object[]
+                {
+                    "ABAB",
+                    new int[] { 10,5,10,15 },
+                    new int[] { 0, 15 }
+                },
+                new object[]
+                {
+                    "B",
+                    new int[] { 100 },
+                    new int[] { 100, 0 }
+                },
+                
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataProperty))]
+    public void Bank_Transfer_Tests(string R, int[] V, int[] expected)
+    {
+        var result = new BankTransfer().InitialAmount(R, V);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Problems/Microsoft/BankTransfer.cs
@@ -0,0 +1,36 @@
+﻿namespace Problems.Microsoft;
+
+public class BankTransfer
+{
+    /// <summary>
+    /// Given a series of transactions from bank A to bank B
+    /// Provide the minimum balance for bank A and bank B
+    /// so that they do not go bankrupt. Their balance does not go < 0
+    /// </summary>
+    /// <param name="R">ABBAABBAB</param>
+    /// <param name="V">242368219</param>
+    /// <returns></returns>
+    public int[] InitialAmount(string R, int[] V)
+    {
+        int minA = 0;
+        int minB = 0;
+        int balance = 0;
+        int n = R.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (R[i] == 'A')
+            {
+                balance += V[i];
+                minB = Math.Min(-balance, minB);
+            }
+            else
+            {
+                balance -= V[i];
+                minA = Math.Min(balance, minA);
+            }
+        }
+
+        return new[] {-minA, -minB};
+    }
+}
